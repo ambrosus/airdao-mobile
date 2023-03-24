a4c21c3ebc0030bead6e22881131b3e767ae0f0f
Illia A <illiaa@Illias-MacBook-Pro.local>
2023-03-24 18:00:23 +0200

    added eslint

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +16,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
