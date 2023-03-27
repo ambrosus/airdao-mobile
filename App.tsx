ea40e4d183115f4991ecb3ee5ed5dd57a5e5c2d5
ilyaAir <ilya@airdao.io>
2023-03-27 11:08:28 +0300

    added husky config

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello world</Text>
+                <Text>hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
