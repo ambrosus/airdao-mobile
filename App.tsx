ef368ef357056af243a67c01ef2d0742f952e615
JavidHaji-zada <cavid.hacizade.99@gmail.com>
2023-03-28 17:19:43 -0700

    fix gesture handler view

--- App.tsx
+++ App.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Navigation from '@navigation/NavigationContainer';
-import { useAppInit } from './src/hooks/useAppInit';
+import { useAppInit } from '@hooks/useAppInit';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import { Text } from './src/components/base';
 
 export default function App() {
   const { isAppReady } = useAppInit();
@@ -14,11 +12,9 @@ export default function App() {
   }
 
   return (
-    <SafeAreaProvider>
-      <GestureHandlerRootView>
+    <SafeAreaProvider style={{ flex: 1 }}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
         <Navigation />
-        <Text heading>Hello World!</Text>
-        <StatusBar style="auto" />
       </GestureHandlerRootView>
     </SafeAreaProvider>
   );
