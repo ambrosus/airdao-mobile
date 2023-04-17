d0374e0c28e33c3982605fd1680c75e502203446
Javid <cavid.hacizade.99@gmail.com>
2023-04-17 13:22:13 +0400

    added usePersonalList hook

--- src/hooks/cache/useWatchlist.ts
+++ src/hooks/cache/useWatchlist.ts
@@ -9,36 +9,17 @@ export const useWatchlist = () => {
     const newAddress = Object.assign({}, address);
     newAddress.isOnWatchlist = true;
     allAddressesReducer({ type: 'add-or-update', payload: newAddress });
-    // const watchlist = await getWatchlist();
-    // watchlist.push(address);
-    // await Cache.setItem(CacheKey.Watchlist, watchlist);
-    // await getWatchlist();
   };
 
-  const getWatchlist = async () => {
-    // const stringifiedWatchlist = (await Cache.getItem(
-    //   CacheKey.Watchlist
-    // )) as ListsOfAddressType[];
-    // let watchlist: ListsOfAddressType[];
-    // if (!stringifiedWatchlist) watchlist = [];
-    // else watchlist = stringifiedWatchlist;
-    // setWatchlist(watchlist);
-    // return watchlist;
-  };
-
-  const updateWalletInList = async () => {
-    // const watchlist = await getWatchlist();
-    // const idx = watchlist.indexOfItem(wallet, 'addressId');
-    // if (idx === -1) return;
-    // watchlist.splice(idx, 1, wallet);
-    // await Cache.setItem(CacheKey.Watchlist, watchlist);
-    // await getWatchlist();
+  const removeFromWatchlist = async (address: ExplorerAccount) => {
+    const newAddress = Object.assign({}, address);
+    newAddress.isOnWatchlist = false;
+    allAddressesReducer({ type: 'add-or-update', payload: newAddress });
   };
 
   return {
     watchlist: allAddresses.filter((account) => account.isOnWatchlist),
     addToWatchlist,
-    updateWalletInList,
-    getWatchlist
+    removeFromWatchlist
   };
 };
