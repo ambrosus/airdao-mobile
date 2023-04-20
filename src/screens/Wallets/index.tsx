989bbd1c1e64bd5f7b3106f6fcd691f3faad4c95
Javid <cavid.hacizade.99@gmail.com>
2023-04-20 13:21:07 +0400

    Wallet Screen: show total "My Wallet Value"

--- src/screens/Wallets/index.tsx
+++ src/screens/Wallets/index.tsx
@@ -7,14 +7,21 @@ import {
   Wallets,
   Watchlists
 } from './components';
-import { styles } from './styles';
 import { useAMBPrice } from '@hooks/query';
 import { useOnboardingStatus } from '@contexts/OnBoardingUserContext';
 import { WalletsFloatButton } from '@screens/Wallets/components/WalletsFloatButton';
+import { usePersonalList } from '@hooks/cache';
+import { styles } from './styles';
 
 export const WalletsScreen = () => {
-  const { data } = useAMBPrice();
+  const { data: ambTokenData } = useAMBPrice();
   const { status, handleStepChange } = useOnboardingStatus((v) => v);
+  const { personalList } = usePersonalList();
+  const ambBalance = personalList.reduce(
+    (prev, curr) => prev + curr.ambBalance,
+    0
+  );
+  const USDBalance = ambBalance * (ambTokenData?.priceUSD || 0);
 
   return (
     <View style={{ flex: 1 }}>
@@ -24,11 +31,11 @@ export const WalletsScreen = () => {
       >
         <StatusBar style="light" backgroundColor="#222222" />
         <PortfolioBalance
-          USDBalance={3900}
-          AMBBalance={1}
+          USDBalance={USDBalance}
+          AMBBalance={ambBalance}
           balanceLast24HourChange={3.46}
-          AMBPriceLast24HourChange={data?.percentChange24H || NaN}
-          AMBPrice={data?.priceUSD || NaN}
+          AMBPriceLast24HourChange={ambTokenData?.percentChange24H || NaN}
+          AMBPrice={ambTokenData?.priceUSD || NaN}
         />
         <View style={styles.content}>
           <Wallets />
