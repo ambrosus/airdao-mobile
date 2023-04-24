9d0d0c9218199ce67f60362991894fa67fdbe1b6
Illia A <illiaa@Illias-MacBook-Pro.local>
2023-04-24 18:09:49 +0300

    added totalAmount

--- src/screens/Lists/components/ListsScreenHeader/index.tsx
+++ src/screens/Lists/components/ListsScreenHeader/index.tsx
@@ -10,8 +10,9 @@ import { FilterIcon } from '@components/svg/icons/Filter';
 import { SettingsIcon } from '@components/svg/icons/Settings';
 import { ProgressArrowIcon } from '@components/svg/icons/ProgressArrow';
 import { styles } from '@screens/Lists/components/ListsScreenHeader/styles';
+import { NumberUtils } from '@utils/number';
 
-export const ListsScreenHeader = () => {
+export const ListsScreenHeader = ({ totalAmount }: { totalAmount: number }) => {
   const filterRef = useRef<BottomSheetRef>(null);
   const handleOpenFilter = useCallback(() => {
     filterRef.current?.show();
@@ -45,7 +46,9 @@ export const ListsScreenHeader = () => {
       <Spacer value={9} />
       <Text style={styles.balanceSubtitle}>Total list balance</Text>
       <Spacer value={15} />
-      <Text style={styles.balanceCount}>$3,900.90</Text>
+      <Text style={styles.balanceCount}>
+        ${NumberUtils.formatNumber(totalAmount, 0)}
+      </Text>
       <View style={styles.balanceStats}>
         <Text
           style={[
