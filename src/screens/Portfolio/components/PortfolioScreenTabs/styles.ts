4490c1856ddd21e5fe9b3a6bcedbf4b6a57ed7df
illiaa <ilya@airdao.io>
2023-05-24 18:48:44 +0300

    added fix

--- src/screens/Portfolio/components/PortfolioScreenTabs/styles.ts
+++ src/screens/Portfolio/components/PortfolioScreenTabs/styles.ts
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native';
+import { COLORS } from '@constants/colors';
 
 export const styles = StyleSheet.create({
   container: {
@@ -14,5 +15,16 @@ export const styles = StyleSheet.create({
     justifyContent: 'center',
     height: 45,
     width: 138
+  },
+  androidButton: {
+    width: 48,
+    height: 48,
+    backgroundColor: COLORS.electricBlue,
+    borderRadius: 50,
+    position: 'absolute',
+    alignSelf: 'flex-end',
+    bottom: '18%',
+    right: '4%',
+    zIndex: 10
   }
 });
