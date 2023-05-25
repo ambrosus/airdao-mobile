ac2920544101df9f259a8864cb43674c02762ce3
JavidHaji-zada <cavid.hacizade.99@gmail.com>
2023-05-25 18:54:07 +0400

    status bar color fix

--- src/components/templates/StatusBar.tsx
+++ src/components/templates/StatusBar.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import { useCurrentRoute } from '@contexts/Navigation';
 import { COLORS } from '@constants/colors';
 
 export const StatusBar = () => {
-  const currentRoute = useCurrentRoute();
-  return (
-    <ExpoStatusBar
-      style={currentRoute === 'HomeScreen' ? 'dark' : 'light'}
-      backgroundColor={currentRoute === 'HomeScreen' ? '#f3f5f7' : COLORS.white}
-    />
-  );
+  return <ExpoStatusBar style={'dark'} backgroundColor={COLORS.white} />;
 };
