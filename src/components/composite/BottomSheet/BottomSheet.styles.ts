6ecfb8102558866fc6f17b3d1658819178c9e792
Javid <cavid.hacizade.99@gmail.com>
2023-05-30 11:34:06 +0400

    BottomSheet styling merge issue fix

--- src/components/composite/BottomSheet/BottomSheet.styles.ts
+++ src/components/composite/BottomSheet/BottomSheet.styles.ts
@@ -3,8 +3,8 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
-    marginBottom: 10,
-    marginHorizontal: 5,
+    marginBottom: 0,
+    marginHorizontal: 0,
     justifyContent: 'flex-end'
   },
   swiper: {
