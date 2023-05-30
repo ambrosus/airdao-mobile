e3ebebfdfa7a3b5bc8761a36fcc3bc548e077a12
Javid <cavid.hacizade.99@gmail.com>
2023-05-30 11:26:11 +0400

    Camera Permission flow fixed

---
+++ src/components/templates/BarcodeScanner/styles.ts
@@ -0,0 +1,17 @@
+import { COLORS } from '@constants/colors';
+import { scale, verticalScale } from '@utils/scaling';
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  noAccessContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  getAccessBtn: {
+    marginTop: verticalScale(4),
+    backgroundColor: COLORS.mainBlue,
+    paddingVertical: verticalScale(8),
+    paddingHorizontal: scale(12)
+  }
+});
