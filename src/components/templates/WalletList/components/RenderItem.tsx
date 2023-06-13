4c832afea09997d890b70b21216e24d850595507
illiaa <ilya@airdao.io>
2023-06-13 15:37:35 +0300

    fixed bug with address swipe in Portfolio

--- src/components/templates/WalletList/components/RenderItem.tsx
+++ src/components/templates/WalletList/components/RenderItem.tsx
@@ -1,11 +1,11 @@
-import React, { useCallback, useRef, useState } from 'react';
+import React, { forwardRef, memo, useCallback, useRef } from 'react';
 import { COLORS } from '@constants/colors';
 import { scale } from '@utils/scaling';
-import { View } from 'react-native';
+import { Animated, Pressable } from 'react-native';
 import { Button, Spacer } from '@components/base';
 import { EditIcon } from '@components/svg/icons';
 import { RemoveIcon } from '@components/svg/icons/Remove';
-import Swipeable from 'react-native-gesture-handler/Swipeable';
+import { Swipeable } from 'react-native-gesture-handler';
 import { WalletItem } from '@components/templates/WalletItem';
 import { ExplorerAccount } from '@models';
 import { useNavigation } from '@react-navigation/native';
@@ -22,100 +22,131 @@ export type SwipeableWalletItemProps = {
   isPortfolioFlow?: boolean;
   removeType?: 'watchlist' | 'collection';
 };
-export const SwipeableWalletItem = ({
-  item,
-  idx,
-  isPortfolioFlow = false,
-  removeType = 'watchlist'
-}: SwipeableWalletItemProps) => {
-  const confirmRemoveRef = useRef<BottomSheetRef>(null);
-  const navigation = useNavigation<WalletsNavigationProp>();
-  const [disabled, setDisabled] = useState<boolean>(false);
 
-  const handleConfirmRemove = useCallback(() => {
-    confirmRemoveRef.current?.show();
-  }, []);
-  const navigateToAddressDetails = () => {
-    navigation.navigate('Address', { address: item.address });
-  };
-  const stylesForPortfolio = isPortfolioFlow
-    ? {
-        paddingVertical: 18,
-        borderColor: COLORS.charcoal,
-        borderBottomWidth: 1,
-        borderTopWidth: idx === 0 ? 1 : 0
-      }
-    : {};
+interface SwipeActionsProps {
+  dragX: Animated.AnimatedInterpolation<number>;
+  onPress?: () => void;
+}
 
-  const editModalRef = useRef<BottomSheetRef>(null);
-  const swipeable = useRef<Swipeable>(null);
+export const SwipeableWalletItem = memo(
+  forwardRef<Swipeable, SwipeableWalletItemProps>(
+    (
+      { item, idx, isPortfolioFlow = false, removeType = 'watchlist' },
+      previousRef
+    ) => {
+      const editModalRef = useRef<BottomSheetRef>(null);
+      const swipeRef = useRef<Swipeable>(null);
+      const confirmRemoveRef = useRef<BottomSheetRef>(null);
+      const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
-  const showEdit = () => {
-    editModalRef.current?.show();
-    swipeable.current?.close();
-  };
+      const navigation = useNavigation<WalletsNavigationProp>();
 
-  const renderRightActions = () => {
-    return (
-      <>
-        <View style={styles.rightActions}>
-          <Button onPress={showEdit}>
-            <EditIcon scale={1.5} color={COLORS.electricBlue} />
-          </Button>
-          <Spacer horizontal value={scale(52)} />
-          <Button onPress={handleConfirmRemove}>
-            <RemoveIcon />
-          </Button>
-        </View>
-        {removeType === 'watchlist' && (
-          <BottomSheetRemoveAddressFromWatchlists
-            item={item}
-            ref={confirmRemoveRef}
-          />
-        )}
-        {removeType === 'collection' && (
-          <BottomSheetRemoveAddressFromCollection
-            wallet={item}
-            ref={confirmRemoveRef}
-          />
-        )}
-      </>
-    );
-  };
+      const handleConfirmRemove = useCallback(() => {
+        confirmRemoveRef.current?.show();
+      }, []);
 
-  return (
-    <Swipeable
-      ref={swipeable}
-      enabled={isPortfolioFlow}
-      renderRightActions={renderRightActions}
-      onBegan={() => {
-        setTimeout(() => {
-          setDisabled(true);
-        }, 80);
-      }}
-      onEnded={() => {
-        setDisabled(false);
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: 'white'
-        }}
-      >
-        <Button
-          key={idx}
-          style={[{ ...styles.item }, stylesForPortfolio]}
-          onPress={() => {
-            if (!disabled) {
-              navigateToAddressDetails();
-            }
-          }}
-          testID={`WalletItem_${idx}`}
+      const navigateToAddressDetails = useCallback(() => {
+        if (timeoutRef.current !== null) {
+          clearTimeout(timeoutRef.current);
+        }
+        timeoutRef.current = setTimeout(() => {
+          navigation.navigate('Address', { address: item.address });
+        }, 200);
+      }, [item.address, navigation, timeoutRef]);
+
+      const stylesForPortfolio = isPortfolioFlow
+        ? {
+            paddingVertical: 18,
+            borderColor: COLORS.charcoal,
+            borderBottomWidth: 1,
+            borderTopWidth: idx === 0 ? 1 : 0
+          }
+        : {};
+
+      const showEdit = () => {
+        editModalRef.current?.show();
+        swipeRef.current?.close();
+      };
+
+      const handleSwipeableOpen = useCallback(() => {
+        clearTimeout(timeoutRef.current ?? undefined);
+        if (previousRef && typeof previousRef !== 'function') {
+          previousRef.current = swipeRef.current;
+        }
+      }, [previousRef, timeoutRef]);
+
+      const handleSwipeableWillOpen = useCallback(() => {
+        clearTimeout(timeoutRef.current ?? undefined);
+        if (
+          previousRef &&
+          typeof previousRef !== 'function' &&
+          previousRef.current !== null
+        ) {
+          if (previousRef.current !== swipeRef.current) {
+            previousRef.current?.close();
+          }
+        }
+      }, [previousRef, timeoutRef]);
+
+      const SwipeAction: React.FC<SwipeActionsProps> = ({ dragX, onPress }) => {
+        const trans = dragX.interpolate({
+          inputRange: [-100, 0],
+          outputRange: [0, 20],
+          extrapolate: 'clamp'
+        });
+
+        return (
+          <>
+            <Pressable style={styles.rightActions} onPress={onPress}>
+              <Animated.View
+                style={[
+                  styles.rightActions,
+                  { backgroundColor: 'transparent' },
+                  { transform: [{ translateX: trans }] }
+                ]}
+              >
+                <Button onPress={showEdit}>
+                  <EditIcon scale={1.5} color={COLORS.electricBlue} />
+                </Button>
+                <Spacer horizontal value={scale(52)} />
+                <Button onPress={handleConfirmRemove}>
+                  <RemoveIcon />
+                </Button>
+              </Animated.View>
+            </Pressable>
+            {removeType === 'watchlist' && (
+              <BottomSheetRemoveAddressFromWatchlists
+                item={item}
+                ref={confirmRemoveRef}
+              />
+            )}
+            {removeType === 'collection' && (
+              <BottomSheetRemoveAddressFromCollection
+                wallet={item}
+                ref={confirmRemoveRef}
+              />
+            )}
+          </>
+        );
+      };
+
+      return (
+        <Swipeable
+          enabled={isPortfolioFlow}
+          renderRightActions={(_, dragX) => <SwipeAction dragX={dragX} />}
+          ref={swipeRef}
+          onSwipeableOpen={handleSwipeableOpen}
+          onSwipeableWillOpen={handleSwipeableWillOpen}
         >
-          <WalletItem item={item} />
-        </Button>
-      </View>
-      <BottomSheetEditWallet wallet={item} ref={editModalRef} />
-    </Swipeable>
-  );
-};
+          <Pressable
+            style={[{ ...styles.item }, stylesForPortfolio]}
+            onPress={navigateToAddressDetails}
+          >
+            <WalletItem item={item} />
+          </Pressable>
+          <BottomSheetEditWallet wallet={item} ref={editModalRef} />
+        </Swipeable>
+      );
+    }
+  )
+);
