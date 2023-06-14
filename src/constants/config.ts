b448b322939922bc3b70896d79104a2cbfe1b686
JavidHaji-zada <cavid.hacizade.99@gmail.com>
2023-06-14 13:10:43 +0400

    clean package.json

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -3,9 +3,9 @@ import * as Updates from 'expo-updates';
 const Config = {
   CMC_API_URL: 'https://sandbox-api.coinmarketcap.com',
   EXPLORER_API_URL: 'https://explorer-api.ambrosus.io',
-  WALLET_API_URL: 'https://wallet-api-api.ambrosus-dev.io'
+  WALLET_API_URL: 'https://wallet-api-api.ambrosus-test.io'
+  // WALLET_API_URL: 'https://wallet-api-api.ambrosus-dev.io'
 };
-
 if (Updates.channel === 'main') {
   Config.WALLET_API_URL = 'https://wallet-api-api.ambrosus.io';
 } else if (Updates.channel === 'stage') {
