62ff61372a351076fde56bf99e3658c3e817d753
JavidHaji-zada <cavid.hacizade.99@gmail.com>
2023-06-16 15:08:02 +0400

    fix explorer api, test env

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -3,13 +3,14 @@ import * as Updates from 'expo-updates';
 const Config = {
   CMC_API_URL: 'https://sandbox-api.coinmarketcap.com',
   EXPLORER_API_URL: 'https://explorer-api.ambrosus.io',
-  WALLET_API_URL: 'https://wallet-api.ambrosus-test.io'
-  // WALLET_API_URL: 'https://wallet-api-api.ambrosus-dev.io'
+  WALLET_API_URL: 'https://wallet-api.ambrosus.io'
 };
 if (Updates.channel === 'main') {
   Config.WALLET_API_URL = 'https://wallet-api-api.ambrosus.io';
+  Config.EXPLORER_API_URL = 'https://explorer-api.ambrosus.io';
 } else if (Updates.channel === 'stage') {
   Config.WALLET_API_URL = 'https://wallet-api.ambrosus-test.io';
+  Config.EXPLORER_API_URL = 'https://explorer-api.ambrosus-test.io';
 }
 
 export default Config;
