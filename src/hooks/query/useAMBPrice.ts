c21c03111a05f0a9682ca5446b424bf05dc71e4f
JavidHaji-zada <cavid.hacizade.99@gmail.com>
2023-06-19 16:21:50 +0400

    fix require cycle & navigate to portfolio from home watchlist & collections

--- src/hooks/query/useAMBPrice.ts
+++ src/hooks/query/useAMBPrice.ts
@@ -1,12 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
 import { QueryResponse } from '@appTypes/QueryResponse';
-import { getAMBTokenData } from '@api/api';
+import { API } from '@api/api';
 import { AMBToken, AMBTokenDTO } from '@models/index';
 
 export function useAMBPrice(): QueryResponse<AMBToken | undefined> {
   const { data, isLoading, error } = useQuery<AMBTokenDTO>(
     ['amb-token'],
-    getAMBTokenData
+    API.getAMBTokenData
   );
 
   return {
