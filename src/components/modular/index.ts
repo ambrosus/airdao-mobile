24669e270701c7d0e3cbf928b38df54c32a4138c
JavidHaji-zada <cavid.hacizade.99@gmail.com>
2023-06-19 19:49:26 +0400

    fixed navigation bug in Home Tabs

--- src/components/modular/index.ts
+++ src/components/modular/index.ts
@@ -1,4 +1,5 @@
 export * from './BottomSheetWithHeader';
 export * from './BottomSheetFloat';
+export * from './CollectionItem';
 export * from './TransactionItem';
 export * from './Toast';
--- src/screens/Wallets/components/HomeTabs/HomeTabs.tsx
+++ src/screens/Wallets/components/HomeTabs/HomeTabs.tsx
@@ -1,51 +1,125 @@
-import { Dimensions, View } from 'react-native';
-import React, { useState } from 'react';
-import { TabView } from 'react-native-tab-view';
-import { HomeTabsScenes } from '@screens/Wallets/components/HomeTabs/HomeTabsScenes';
+import { Dimensions, ScrollView, View } from 'react-native';
+import React, { useCallback, useRef, useState } from 'react';
 import { HomeWatchlists } from '@screens/Wallets/components/HomeTabs/HomeWatchlists';
 import { HomeCollections } from '@screens/Wallets/components/HomeTabs/HomeCollections';
-import type { Props as TabViewProps } from 'react-native-tab-view/lib/typescript/src/TabView';
 import { styles } from '@screens/Wallets/components/HomeTabs/styles';
-
-const homeTabRoutes = [
-  { key: 'first', title: 'Watchlists' },
-  { key: 'second', title: 'Collections' }
-] as const;
-
-type HomeTabRoutes = typeof homeTabRoutes;
-
-type HomeTabViewRoute = {
-  key: HomeTabRoutes[number]['key'];
-  title: HomeTabRoutes[number]['title'];
-};
-
-type RenderSceneProps = Parameters<
-  TabViewProps<HomeTabViewRoute>['renderScene']
->[0];
-const renderScene = ({ route }: RenderSceneProps) => {
-  switch (route.key) {
-    case 'first':
-      return <HomeWatchlists />;
-    case 'second':
-      return <HomeCollections />;
-    default:
-      return null;
-  }
-};
+import { Button, Row, Spacer, Text } from '@components/base';
+import { scale, verticalScale } from '@utils/scaling';
+import { AddIcon } from '@components/svg/icons/AddIcon';
+import { COLORS } from '@constants/colors';
+import { BottomSheetCreateCollectionOrAddAddress } from '@components/templates/BottomSheetCreateCollectionOrAddAddress';
+import { BottomSheetRef } from '@components/composite';
+import { SearchTabNavigationProp } from '@appTypes';
+import { useNavigation } from '@react-navigation/native';
+import { BottomSheetCreateRenameGroup } from '@components/templates/BottomSheetCreateRenameGroup';
+import { useLists } from '@contexts';
 
 export const HomeTabs = () => {
-  const [index, setIndex] = useState(0);
-  const [routes] = useState<HomeTabViewRoute[]>(
-    homeTabRoutes as unknown as HomeTabViewRoute[]
-  );
+  const navigation = useNavigation<SearchTabNavigationProp>();
+  const { handleOnCreate } = useLists((v) => v);
 
-  const numTabs = 4;
-  const numItems = Math.min(numTabs, HomeTabsScenes.length);
-  const height = numItems * (Dimensions.get('screen').height * 0.45) + 100;
+  const scrollView = useRef<ScrollView>(null);
+  const createCollectionOrAddAddressRef = useRef<BottomSheetRef>(null);
+  const createRenameGroupRef = useRef<BottomSheetRef>(null);
+  const tabWidth = Dimensions.get('window').width - scale(32);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleOnCreateCollectionOrAddAddress = useCallback(() => {
+    createCollectionOrAddAddressRef.current?.show();
+  }, []);
+
+  const handleOnAddNewAddress = useCallback(() => {
+    createCollectionOrAddAddressRef.current?.dismiss();
+
+    setTimeout(
+      () => navigation.navigate('Search', { screen: 'SearchScreen' }),
+      400
+    );
+  }, [navigation]);
+
+  const handleCreateCollectionPress = useCallback(() => {
+    createCollectionOrAddAddressRef.current?.dismiss();
+    setTimeout(() => createRenameGroupRef.current?.show(), 400);
+  }, []);
+
+  const seeAll = useCallback(() => {
+    navigation.navigate('Portfolio', {
+      screen: 'PortfolioScreen',
+      params: {
+        tabs: { activeTab: currentIndex }
+      }
+    });
+  }, [currentIndex, navigation]);
+
+  const scrollToWatchlist = () => {
+    scrollView.current?.scrollTo({ x: 0, animated: true });
+    setCurrentIndex(0);
+  };
+
+  const scrollToCollections = () => {
+    scrollView.current?.scrollTo({ x: tabWidth, animated: true });
+    setCurrentIndex(1);
+  };
+
+  // TODO implement nice color animation switch when index changes
 
   return (
-    <View style={[styles.homeTabs, { height }]} testID="lists-screen">
-      <TabView<HomeTabViewRoute>
+    <View style={styles.homeTabs}>
+      <Row
+        alignItems="center"
+        justifyContent="space-between"
+        style={{
+          paddingHorizontal: scale(24)
+        }}
+      >
+        <Row alignItems="center">
+          <Button onPress={scrollToWatchlist}>
+            <Text
+              fontFamily="Inter_700Bold"
+              color={currentIndex === 0 ? COLORS.jetBlack : COLORS.lavenderGray}
+              fontSize={currentIndex === 0 ? 20 : 18}
+            >
+              Watchlist
+            </Text>
+          </Button>
+          <Spacer value={scale(16)} horizontal />
+          <Button onPress={scrollToCollections}>
+            <Text
+              fontFamily="Inter_700Bold"
+              color={currentIndex === 1 ? COLORS.jetBlack : COLORS.lavenderGray}
+              fontSize={currentIndex === 1 ? 20 : 18}
+            >
+              Collections
+            </Text>
+          </Button>
+        </Row>
+        <Button
+          type="circular"
+          onPress={handleOnCreateCollectionOrAddAddress}
+          style={styles.addButton}
+        >
+          <AddIcon color={COLORS.white} scale={0.8} />
+        </Button>
+      </Row>
+      <Spacer value={verticalScale(28)} />
+      <ScrollView
+        ref={scrollView}
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        testID="lists-screen"
+        onMomentumScrollEnd={(event) => {
+          const scrollOffsetX = event.nativeEvent.contentOffset.x;
+          setCurrentIndex(scrollOffsetX > 0 ? 1 : 0);
+        }}
+      >
+        <View style={{ width: tabWidth }}>
+          <HomeWatchlists />
+        </View>
+        <View style={{ width: tabWidth }}>
+          <HomeCollections />
+        </View>
+        {/* <TabView<HomeTabViewRoute>
         style={styles.tabView}
         navigationState={{ index, routes }}
         renderScene={renderScene}
@@ -55,6 +129,27 @@ export const HomeTabs = () => {
             <HomeTabsScenes {...props} onIndexChange={setIndex} index={index} />
           );
         }}
+      /> */}
+      </ScrollView>
+      <Spacer value={verticalScale(24)} />
+      <Button onPress={seeAll} style={styles.seeAllButton}>
+        <Text
+          fontFamily="Inter_600SemiBold"
+          fontSize={16}
+          color={COLORS.deepBlue}
+        >
+          See all
+        </Text>
+      </Button>
+      <BottomSheetCreateCollectionOrAddAddress
+        handleOnAddNewAddress={handleOnAddNewAddress}
+        handleCreateCollectionPress={handleCreateCollectionPress}
+        ref={createCollectionOrAddAddressRef}
+      />
+      <BottomSheetCreateRenameGroup
+        type="create"
+        ref={createRenameGroupRef}
+        handleOnCreateGroup={handleOnCreate}
       />
     </View>
   );
