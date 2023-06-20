fa549661be5b355bdd36d3bc3b9e98efaa9d41ff
illiaa <ilya@airdao.io>
2023-06-20 17:16:34 +0300

    added button for clearing cache

--- src/screens/Settings/components/SettingsInfoBlock/index.tsx
+++ src/screens/Settings/components/SettingsInfoBlock/index.tsx
@@ -1,27 +1,39 @@
 import { Button, Row, Spacer, Text } from '@components/base';
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { View } from 'react-native';
 import { HelpIcon } from '@components/svg/icons/Help';
 import { AppStoreIcon } from '@components/svg/icons/AppStore';
 import { styles } from './style';
+import { ClearCacheModal } from '@screens/Settings/components/SettingsInfoBlock/ClearCacheModal';
+import { BottomSheetRef } from '@components/composite';
 
 export const SettingsInfoBlock = () => {
+  const clearCacheModalRef = useRef<BottomSheetRef>(null);
+  const openClearCacheModal = useCallback(() => {
+    clearCacheModalRef.current?.show();
+  }, []);
+
   return (
-    <View testID="setting-screen_settings-info-block">
-      <Spacer value={42} />
-      <Button type="base">
-        <Row>
-          <HelpIcon />
-          <Text style={styles.infoTextContainer}>Help center</Text>
-        </Row>
-      </Button>
-      <Spacer value={43} />
-      <Button type="base">
-        <Row>
-          <AppStoreIcon />
-          <Text style={styles.infoTextContainer}>Rate us on the App Store</Text>
-        </Row>
-      </Button>
-    </View>
+    <>
+      <View testID="setting-screen_settings-info-block">
+        <Spacer value={42} />
+        <Button type="base" onLongPress={openClearCacheModal}>
+          <Row>
+            <HelpIcon />
+            <Text style={styles.infoTextContainer}>Help center</Text>
+          </Row>
+        </Button>
+        <Spacer value={43} />
+        <Button type="base">
+          <Row>
+            <AppStoreIcon />
+            <Text style={styles.infoTextContainer}>
+              Rate us on the App Store
+            </Text>
+          </Row>
+        </Button>
+      </View>
+      <ClearCacheModal ref={clearCacheModalRef} />
+    </>
   );
 };
