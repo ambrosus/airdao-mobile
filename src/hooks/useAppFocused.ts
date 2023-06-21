8337afa4751627388195564c38e7dee562448f68
JavidHaji-zada <cavid.hacizade.99@gmail.com>
2023-06-21 12:26:29 +0400

    fixed new notification count check cases

---
+++ src/hooks/useAppFocused.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+import { useIsFocused } from '@react-navigation/native';
+import { useAppState } from './useAppState';
+
+const useAppFocus = () => {
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+  const navigationIsFocused = useIsFocused();
+  const appState = useAppState();
+
+  useEffect(() => {
+    setIsFocused(navigationIsFocused && appState === 'active');
+  }, [navigationIsFocused, appState]);
+
+  return isFocused;
+};
+
+export default useAppFocus;
