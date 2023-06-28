d197144980aaeff2e5a527b1db36ea5538e923af
illiaa <ilya@airdao.io>
2023-06-28 14:08:38 +0300

    fixed Portfolio, Search and Settings screens flickering

--- src/screens/Settings/index.tsx
+++ src/screens/Settings/index.tsx
@@ -4,19 +4,16 @@ import { SettingsBlock } from '@screens/Settings/components/SettingsBlock';
 import { COLORS } from '@constants/colors';
 import { SettingsInfoBlock } from '@screens/Settings/components/SettingsInfoBlock';
 import { scale } from '@utils/scaling';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export const SettingsScreen = () => {
+  const { top } = useSafeAreaInsets();
   return (
-    <SafeAreaView
-      edges={['top']}
-      style={styles.container}
-      testID="settings-screen"
-    >
+    <View style={[{ top }, styles.container]} testID="settings-screen">
       <SettingsBlock />
       <View style={styles.separator} />
       <SettingsInfoBlock />
-    </SafeAreaView>
+    </View>
   );
 };
 
