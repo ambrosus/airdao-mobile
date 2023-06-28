34743637f1c5e87bc85763b886b23d6437e3bed6
illiaa <ilya@airdao.io>
2023-06-28 12:20:24 +0300

    fixed [ANDROID ONLY] Selected Currency and Language row alignment

--- src/screens/Settings/components/SettingsBlock/index.tsx
+++ src/screens/Settings/components/SettingsBlock/index.tsx
@@ -49,81 +49,37 @@ export const SettingsBlock = () => {
   return (
     <View testID="settings-screen_settings-block">
       {Platform.OS === 'ios' ? <Spacer value={25} /> : <Spacer value={42} />}
-      {Platform.OS === 'ios' ? (
-        <View testID="bottom-sheet-select-base-currency">
-          <Button onPress={handleOnSelectBaseCurrency} type="base">
-            <Row
-              style={styles.optionContainer}
-              justifyContent="space-between"
-              alignItems="center"
-            >
-              <Row style={styles.infoTextContainer} alignItems="center">
-                <CurrencyIcon />
-                <Text style={styles.optionInfoText}>Base currency</Text>
-              </Row>
-              <Row style={styles.infoTextContainer} alignItems="center">
-                <Text style={styles.optionButtonText}>{selectedCurrency}</Text>
-                <ChevronRightIcon color={COLORS.smokyBlack} />
-              </Row>
+      <View testID="bottom-sheet-select-base-currency">
+        <Button onPress={handleOnSelectBaseCurrency} type="base">
+          <Row
+            style={styles.optionContainer}
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Row style={styles.infoTextContainer}>
+              <CurrencyIcon />
+              <Text style={styles.optionInfoText}>Base currency</Text>
             </Row>
-          </Button>
-          <Spacer value={20} />
-        </View>
-      ) : (
-        <View testID="bottom-sheet-select-base-currency">
-          <Button onPress={handleOnSelectBaseCurrency} type="base">
-            <View
-              style={{
-                paddingBottom: 35,
-                flexDirection: 'column',
-                alignItems: 'flex-start'
-              }}
-            >
-              <Row alignItems="center" style={styles.infoTextContainer}>
-                <CurrencyIcon />
-                <Text style={styles.optionInfoText}>Base currency</Text>
-              </Row>
-              <Spacer value={10} />
+            <Row style={styles.infoTextContainer} alignItems="center">
               <Text style={styles.optionButtonText}>{selectedCurrency}</Text>
-            </View>
-          </Button>
-        </View>
-      )}
-      {Platform.OS === 'ios' ? (
-        <View testID="bottom-sheet-select-language">
-          <Button onPress={handleOnOpenLanguageModal} type="base">
-            <Row
-              style={styles.optionContainer}
-              justifyContent="space-between"
-              alignItems="center"
-            >
-              <Row alignItems="center" style={styles.infoTextContainer}>
-                <LanguageIcon />
-                <Text style={styles.optionInfoText}>Language</Text>
-              </Row>
-              <Row style={styles.infoTextContainer} alignItems="center">
-                <Text style={styles.optionButtonText}>{selectedLanguage}</Text>
-                <ChevronRightIcon color={COLORS.smokyBlack} />
-              </Row>
+              <ChevronRightIcon color={COLORS.smokyBlack} />
             </Row>
-          </Button>
-          <Spacer value={20} />
-        </View>
-      ) : (
-        <View testID="bottom-sheet-select-language">
-          <Button onPress={handleOnOpenLanguageModal} type="base">
-            <View
-              style={{
-                paddingBottom: 35,
-                flexDirection: 'column',
-                alignItems: 'flex-start'
-              }}
-            >
-              <Row alignItems="center" style={styles.infoTextContainer}>
-                <LanguageIcon />
-                <Text style={styles.optionInfoText}>Language</Text>
-              </Row>
-              <Spacer value={10} />
+          </Row>
+        </Button>
+        <Spacer value={20} />
+      </View>
+      <View testID="bottom-sheet-select-language">
+        <Button onPress={handleOnOpenLanguageModal} type="base">
+          <Row
+            style={styles.optionContainer}
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Row style={styles.infoTextContainer}>
+              <LanguageIcon />
+              <Text style={styles.optionInfoText}>Language</Text>
+            </Row>
+            <Row style={styles.infoTextContainer} alignItems="center">
               <Text style={styles.optionButtonText}>{selectedLanguage}</Text>
               <ChevronRightIcon color={COLORS.smokyBlack} />
             </Row>
@@ -142,8 +98,8 @@ export const SettingsBlock = () => {
               <DarkNotificationIcon />
               <Text style={styles.optionInfoText}>Notification settings</Text>
             </Row>
-            <Row alignItems="center" style={styles.infoTextContainer}>
-              <RightArrowIcon />
+            <Row style={styles.infoTextContainer} alignItems="center">
+              <ChevronRightIcon color={COLORS.smokyBlack} />
             </Row>
           </Row>
         </Button>
