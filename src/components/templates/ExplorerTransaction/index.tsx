4a04b0880465c1e3a20bb877bc4561dd67b7c94c
illiaa <ilya@airdao.io>
2023-07-09 14:21:48 +0300

    added style fix

--- src/components/templates/ExplorerTransaction/index.tsx
+++ src/components/templates/ExplorerTransaction/index.tsx
@@ -31,91 +31,81 @@ export const ExplorerTransaction = (props: ExplorerTransactionProps) => {
     : -1;
 
   return (
-    <>
-      <View style={{ flex: 1, paddingHorizontal: 16 }}>
-        <Text fontFamily="Inter_700Bold" fontSize={20} color={COLORS.nero}>
-          Transaction details
+    <View style={{ flex: 1, paddingHorizontal: 16, paddingTop: 25 }}>
+      <Text fontFamily="Inter_700Bold" fontSize={20} color={COLORS.nero}>
+        Transaction details
+      </Text>
+      <Spacer value={verticalScale(24)} />
+      <JustifiedRow>
+        <Text fontFamily="Inter_600SemiBold" fontSize={13} color="#646464">
+          Method
         </Text>
-        <Spacer value={verticalScale(24)} />
-        <JustifiedRow>
-          <Text fontFamily="Inter_600SemiBold" fontSize={13} color="#646464">
-            Method
-          </Text>
-          <Text fontFamily="Inter_600SemiBold" fontSize={16}>
-            {transaction.type}
-          </Text>
-        </JustifiedRow>
-        <Spacer value={ROW_MARGIN} />
-        <JustifiedRow>
-          <Text fontFamily="Inter_600SemiBold" fontSize={13} color="#646464">
-            Amount
-          </Text>
-          <Text fontFamily="Inter_600SemiBold" fontSize={16}>
-            {NumberUtils.formatNumber(transaction.value.ether, 0)} AMB
-            {totalTransactionAmount !== -1 && ` ($${totalTransactionAmount})`}
-          </Text>
-        </JustifiedRow>
-        {transaction.from_id && (
-          <>
-            <Spacer value={ROW_MARGIN} />
-            <JustifiedRow>
-              <Text
-                fontFamily="Inter_600SemiBold"
-                fontSize={13}
-                color="#646464"
-              >
-                From
-              </Text>
-              <Text
-                color={COLORS.mainBlue}
-                fontFamily="Inter_600SemiBold"
-                fontSize={16}
-              >
-                {StringUtils.formatAddress(transaction.from_id.address, 4, 5)}
-              </Text>
-            </JustifiedRow>
-          </>
-        )}
-        {transaction.to_id && (
-          <>
-            <Spacer value={ROW_MARGIN} />
-            <JustifiedRow>
-              <Text
-                fontFamily="Inter_600SemiBold"
-                fontSize={13}
-                color="#646464"
-              >
-                To
-              </Text>
-              <Text
-                color={COLORS.mainBlue}
-                fontFamily="Inter_600SemiBold"
-                fontSize={16}
-              >
-                {StringUtils.formatAddress(transaction.to_id.address, 4, 5)}
-              </Text>
-            </JustifiedRow>
-          </>
-        )}
-        <Spacer value={ROW_MARGIN} />
-        <JustifiedRow>
-          <Text fontFamily="Inter_600SemiBold" fontSize={13} color="#646464">
-            TxFee
-          </Text>
-          <Text color="#222222" fontFamily="Inter_600SemiBold" fontSize={16}>
-            {transaction.gasCost.ether}
-          </Text>
-        </JustifiedRow>
-        <Spacer value={ROW_MARGIN} />
-        <JustifiedRow>
-          <Text fontFamily="Inter_600SemiBold" fontSize={13} color="#646464">
-            Time
-          </Text>
-          <Text fontFamily="Inter_600SemiBold" fontSize={16}>
-            {moment(transaction.timestamp * 1000).fromNow()}
-          </Text>
-        </JustifiedRow>
-      </View>
-    </>
+        <Text fontFamily="Inter_600SemiBold" fontSize={16}>
+          {transaction.type}
+        </Text>
+      </JustifiedRow>
+      <Spacer value={ROW_MARGIN} />
+      <JustifiedRow>
+        <Text fontFamily="Inter_600SemiBold" fontSize={13} color="#646464">
+          Amount
+        </Text>
+        <Text fontFamily="Inter_600SemiBold" fontSize={16}>
+          {NumberUtils.formatNumber(transaction.value.ether, 0)} AMB
+          {totalTransactionAmount !== -1 && ` ($${totalTransactionAmount})`}
+        </Text>
+      </JustifiedRow>
+      {transaction.from_id && (
+        <>
+          <Spacer value={ROW_MARGIN} />
+          <JustifiedRow>
+            <Text fontFamily="Inter_600SemiBold" fontSize={13} color="#646464">
+              From
+            </Text>
+            <Text
+              color={COLORS.mainBlue}
+              fontFamily="Inter_600SemiBold"
+              fontSize={16}
+            >
+              {StringUtils.formatAddress(transaction.from_id.address, 4, 5)}
+            </Text>
+          </JustifiedRow>
+        </>
+      )}
+      {transaction.to_id && (
+        <>
+          <Spacer value={ROW_MARGIN} />
+          <JustifiedRow>
+            <Text fontFamily="Inter_600SemiBold" fontSize={13} color="#646464">
+              To
+            </Text>
+            <Text
+              color={COLORS.mainBlue}
+              fontFamily="Inter_600SemiBold"
+              fontSize={16}
+            >
+              {StringUtils.formatAddress(transaction.to_id.address, 4, 5)}
+            </Text>
+          </JustifiedRow>
+        </>
+      )}
+      <Spacer value={ROW_MARGIN} />
+      <JustifiedRow>
+        <Text fontFamily="Inter_600SemiBold" fontSize={13} color="#646464">
+          TxFee
+        </Text>
+        <Text color="#222222" fontFamily="Inter_600SemiBold" fontSize={16}>
+          {transaction.gasCost.ether}
+        </Text>
+      </JustifiedRow>
+      <Spacer value={ROW_MARGIN} />
+      <JustifiedRow>
+        <Text fontFamily="Inter_600SemiBold" fontSize={13} color="#646464">
+          Time
+        </Text>
+        <Text fontFamily="Inter_600SemiBold" fontSize={16}>
+          {moment(transaction.timestamp * 1000).fromNow()}
+        </Text>
+      </JustifiedRow>
+    </View>
   );
 };
