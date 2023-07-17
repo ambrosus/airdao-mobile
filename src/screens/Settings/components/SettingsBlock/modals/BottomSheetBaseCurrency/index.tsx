6c730fb514892a84e84464ab79cfcdd55e710324
illiaa <ilya@airdao.io>
2023-07-17 17:18:36 +0300

    fixed spacing in base currency and select language modals on settings screen

--- src/screens/Settings/components/SettingsBlock/modals/BottomSheetBaseCurrency/index.tsx
+++ src/screens/Settings/components/SettingsBlock/modals/BottomSheetBaseCurrency/index.tsx
@@ -1,7 +1,7 @@
 import React, { ForwardedRef, forwardRef, RefObject, useState } from 'react';
 import { BottomSheet, BottomSheetRef, Header } from '@components/composite';
 import { Spacer, Text } from '@components/base';
-import { Dimensions, FlatList, Platform } from 'react-native';
+import { Dimensions, FlatList } from 'react-native';
 import { useForwardedRef } from '@hooks/useForwardedRef';
 import { SettingsModalItem } from '@screens/Settings/components/SettingsBlock/components/SettingsModalItem';
 import { styles } from '@screens/Settings/components/SettingsBlock/modals/style';
@@ -82,7 +82,7 @@ export const BottomSheetSelectBaseCurrency = forwardRef<BottomSheetRef, Props>(
         ref={localRef}
         containerStyle={styles.bottomSheet}
       >
-        {Platform.OS === 'ios' && <Spacer value={topInset} />}
+        <Spacer value={topInset} />
         <Header
           title={
             <Text
