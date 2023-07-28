464be7baae4712e98a21478aa35c4d971a4c6f4b
JavidHaji-zada <cavid.hacizade.99@gmail.com>
2023-07-28 13:31:08 +0400

    added plugin to disable build for Android tablets

---
+++ plugins/withSupportsScreenAndroid.js
@@ -0,0 +1,38 @@
+'use strict';
+var __assign =
+  (this && this.__assign) ||
+  function () {
+    __assign =
+      Object.assign ||
+      function (t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+          s = arguments[i];
+          for (var p in s)
+            if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+        }
+        return t;
+      };
+    return __assign.apply(this, arguments);
+  };
+exports.__esModule = true;
+var config_plugins_1 = require('@expo/config-plugins');
+function addAttributesToManifest(androidManifest) {
+  var manifest = androidManifest.manifest;
+  manifest.$['android:smallScreens'];
+  var supportsScreens = {};
+  supportsScreens.$ = __assign(__assign({}, supportsScreens.$), {
+    'android:smallScreens': true,
+    'android:normalScreens': true,
+    'android:largeScreens': false,
+    'android:xlargeScreens': false
+  });
+  manifest['supports-screens'] = supportsScreens;
+  return androidManifest;
+}
+var withSupportsScreens = function (config) {
+  return (0, config_plugins_1.withAndroidManifest)(config, function (config) {
+    config.modResults = addAttributesToManifest(config.modResults);
+    return config;
+  });
+};
+exports['default'] = withSupportsScreens;
