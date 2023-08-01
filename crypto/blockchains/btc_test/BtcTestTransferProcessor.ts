be345545ceabc402a90c9e869c9d75c2a052165a
JavidHaji-zada <cavid.hacizade.99@gmail.com>
2023-08-01 20:27:30 +0400

    converted btc_test and btg

--- crypto/blockchains/btc_test/BtcTestTransferProcessor.ts
+++ crypto/blockchains/btc_test/BtcTestTransferProcessor.ts
@@ -1,39 +1,53 @@
 /**
  * @version 0.52
  */
-import { BlocksoftBlockchainTypes } from '@crypto/blockchains/BlocksoftBlockchainTypes'
-import DogeTransferProcessor from '@crypto/blockchains/doge/DogeTransferProcessor'
-import BtcTestUnspentsProvider from '@crypto/blockchains/btc_test/providers/BtcTestUnspentsProvider'
-import BtcTestSendProvider from '@crypto/blockchains/btc_test/providers/BtcTestSendProvider'
-import DogeTxInputsOutputs from '@crypto/blockchains/doge/tx/DogeTxInputsOutputs'
-import DogeTxBuilder from '@crypto/blockchains/doge/tx/DogeTxBuilder'
+import { AirDAOBlockchainTypes } from '@crypto/blockchains/AirDAOBlockchainTypes';
+import DogeTransferProcessor from '@crypto/blockchains/doge/DogeTransferProcessor';
+import BtcTestUnspentsProvider from '@crypto/blockchains/btc_test/providers/BtcTestUnspentsProvider';
+import BtcTestSendProvider from '@crypto/blockchains/btc_test/providers/BtcTestSendProvider';
+import DogeTxInputsOutputs from '@crypto/blockchains/doge/tx/DogeTxInputsOutputs';
+import DogeTxBuilder from '@crypto/blockchains/doge/tx/DogeTxBuilder';
 
-export default class BtcTestTransferProcessor extends DogeTransferProcessor implements BlocksoftBlockchainTypes.TransferProcessor {
+export default class BtcTestTransferProcessor
+  extends DogeTransferProcessor
+  implements AirDAOBlockchainTypes.TransferProcessor
+{
+  _trezorServerCode = 'NONE';
 
-    _trezorServerCode = 'NONE'
+  _builderSettings: AirDAOBlockchainTypes.BuilderSettings = {
+    minOutputDustReadable: 0.000005,
+    minChangeDustReadable: 0.00001,
+    feeMaxForByteSatoshi: 10000, // for tx builder
+    feeMaxAutoReadable2: 0.2, // for fee calc,
+    feeMaxAutoReadable6: 0.1, // for fee calc
+    feeMaxAutoReadable12: 0.05, // for fee calc
+    changeTogether: true,
+    minRbfStepSatoshi: 10,
+    minSpeedUpMulti: 1.5
+  };
 
-    _builderSettings: BlocksoftBlockchainTypes.BuilderSettings = {
-        minOutputDustReadable: 0.000005,
-        minChangeDustReadable: 0.00001,
-        feeMaxForByteSatoshi: 10000, // for tx builder
-        feeMaxAutoReadable2: 0.2, // for fee calc,
-        feeMaxAutoReadable6: 0.1, // for fee calc
-        feeMaxAutoReadable12: 0.05, // for fee calc
-        changeTogether: true,
-        minRbfStepSatoshi: 10,
-        minSpeedUpMulti : 1.5
-    }
+  canRBF(
+    data: AirDAOBlockchainTypes.DbAccount,
+    transaction: AirDAOBlockchainTypes.DbTransaction
+  ): boolean {
+    return false;
+  }
 
-    canRBF(data: BlocksoftBlockchainTypes.DbAccount, transaction: BlocksoftBlockchainTypes.DbTransaction): boolean {
-        return false
-    }
-
-    _initProviders() {
-        if (this._initedProviders) return false
-        this.unspentsProvider = new BtcTestUnspentsProvider(this._settings, this._trezorServerCode)
-        this.sendProvider = new BtcTestSendProvider(this._settings, this._trezorServerCode)
-        this.txPrepareInputsOutputs = new DogeTxInputsOutputs(this._settings, this._builderSettings)
-        this.txBuilder = new DogeTxBuilder(this._settings, this._builderSettings)
-        this._initedProviders = true
-    }
+  _initProviders() {
+    if (this._initedProviders) return false;
+    this.unspentsProvider = new BtcTestUnspentsProvider(
+      this._settings,
+      this._trezorServerCode
+    );
+    this.sendProvider = new BtcTestSendProvider(
+      this._settings,
+      this._trezorServerCode
+    );
+    this.txPrepareInputsOutputs = new DogeTxInputsOutputs(
+      this._settings,
+      this._builderSettings
+    );
+    this.txBuilder = new DogeTxBuilder(this._settings, this._builderSettings);
+    this._initedProviders = true;
+  }
 }
--- crypto/blockchains/btg/BtgScannerProcessor.js
+++ crypto/blockchains/btg/BtgScannerProcessor.js
@@ -3,19 +3,18 @@
  * https://github.com/trezor/blockbook/blob/master/docs/api.md
  */
 
-import DogeScannerProcessor from '../doge/DogeScannerProcessor'
+import DogeScannerProcessor from '../doge/DogeScannerProcessor';
 
 export default class BtgScannerProcessor extends DogeScannerProcessor {
+  /**
+   * @type {number}
+   * @private
+   */
+  _blocksToConfirm = 10;
 
-    /**
-     * @type {number}
-     * @private
-     */
-    _blocksToConfirm = 10
-
-    /**
-     * @type {string}
-     * @private
-     */
-    _trezorServerCode = 'BTG_TREZOR_SERVER'
+  /**
+   * @type {string}
+   * @private
+   */
+  _trezorServerCode = 'BTG_TREZOR_SERVER';
 }
