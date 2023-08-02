2179387b3db178bd68b335db1f801cc0dd62418b
JavidHaji-zada <cavid.hacizade.99@gmail.com>
2023-08-03 00:50:53 +0400

    converted etc

--- crypto/blockchains/etc/EtcTransferProcessor.ts
+++ crypto/blockchains/etc/EtcTransferProcessor.ts
@@ -2,12 +2,18 @@
  * @author Ksu
  * @version 0.43
  */
-import { BlocksoftBlockchainTypes } from '@crypto/blockchains/BlocksoftBlockchainTypes'
-import BnbSmartTransferProcessor from '@crypto/blockchains/bnb_smart/BnbSmartTransferProcessor'
+import { AirDAOBlockchainTypes } from '@crypto/blockchains/AirDAOBlockchainTypes';
+import BnbSmartTransferProcessor from '@crypto/blockchains/bnb_smart/BnbSmartTransferProcessor';
 
-export default class EtcTransferProcessor extends BnbSmartTransferProcessor implements BlocksoftBlockchainTypes.TransferProcessor {
-
-    canRBF(data: BlocksoftBlockchainTypes.DbAccount, transaction: BlocksoftBlockchainTypes.DbTransaction, source: string): boolean {
-        return false
-    }
+export default class EtcTransferProcessor
+  extends BnbSmartTransferProcessor
+  implements AirDAOBlockchainTypes.TransferProcessor
+{
+  canRBF(
+    data: AirDAOBlockchainTypes.DbAccount,
+    transaction: AirDAOBlockchainTypes.DbTransaction,
+    source: string
+  ): boolean {
+    return false;
+  }
 }
