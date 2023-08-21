4efeccfcbb73c1726d3f46e19c4e65f20eb6d9e4
illiaa <ilya@airdao.io>
2023-08-21 16:52:59 +0300

    Fix localization

--- src/localization/i18n.ts
+++ src/localization/i18n.ts
@@ -3,6 +3,7 @@ import { initReactI18next } from 'react-i18next';
 import English from '../localization/locales/English.json';
 import Turkish from '../localization/locales/Turkish.json';
 
+// Comment to trigger merge
 i18n.use(initReactI18next).init({
   fallbackLng: 'English',
   debug: false,
