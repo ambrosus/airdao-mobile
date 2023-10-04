4ff1def9da97c48e659fab4b680ec5aafd44bf6a
JavidHaji-zada <cavid.hacizade.99@gmail.com>
2023-10-04 13:22:13 +0400

    do not close passcode modal on back press

--- src/components/templates/PasscodeModal/PasscodeModal.tsx
+++ src/components/templates/PasscodeModal/PasscodeModal.tsx
@@ -110,6 +110,7 @@ export const PasscodeModal = forwardRef<
       ref={localRef}
       containerStyle={styles.container}
       height={Dimensions.get('screen').height}
+      closeOnBackPress={false}
     >
       <KeyboardAvoidingView
         enabled
