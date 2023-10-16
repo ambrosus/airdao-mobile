af84a6782b0d2e08951872a9575229dcc67affde
JavidHaji-zada <cavid.hacizade.99@gmail.com>
2023-10-16 10:04:12 +0400

    Confirm passcode: fix translation in title & description

--- src/screens/CreateWallet/components/ConfirmPasscode.tsx
+++ src/screens/CreateWallet/components/ConfirmPasscode.tsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { Spacer, Text, Button } from '@components/base';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { Alert, KeyboardAvoidingView, View } from 'react-native';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
+import { useTranslation } from 'react-i18next';
+import { Spacer, Text, Button } from '@components/base';
 import { scale, verticalScale } from '@utils/scaling';
 import { COLORS } from '@constants/colors';
-import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 import { HomeNavigationProp, HomeParamsList } from '@appTypes';
-import { useTranslation } from 'react-i18next';
 import { PasscodeUtils } from '@utils/passcode';
 import { Passcode } from '@components/modular';
+import { BottomAwareSafeAreaView } from '@components/composite';
 
 export const ConfirmPasscode = () => {
-  const { top } = useSafeAreaInsets();
   const { t } = useTranslation();
   const navigation = useNavigation<HomeNavigationProp>();
   const route = useRoute<RouteProp<HomeParamsList, 'ConfirmPasscode'>>();
@@ -34,56 +34,57 @@ export const ConfirmPasscode = () => {
   };
 
   return (
-    <KeyboardAvoidingView
-      enabled
-      behavior="padding"
-      style={{ top, flex: 1, justifyContent: 'space-between' }}
-    >
-      <View style={{ paddingHorizontal: scale(16) }}>
-        <Spacer value={verticalScale(47)} />
-        <Text
-          align="center"
-          fontSize={24}
-          fontFamily="Inter_700Bold"
-          color={COLORS.neutral800}
-        >
-          Confirm passcode
-        </Text>
-        <Spacer value={verticalScale(8)} />
-        <Text
-          align="center"
-          fontSize={16}
-          fontFamily="Inter_500Medium"
-          color={COLORS.neutral800}
-        >
-          Re-enter your passcode to secure your wallet
-        </Text>
-        <Spacer value={verticalScale(106)} />
-        <Passcode onPasscodeChange={onPasscodeChange} autoFocus={true} />
-        <Spacer value={verticalScale(50)} />
-      </View>
-      <View style={{ paddingHorizontal: scale(16) }}>
-        <Button
-          disabled={!isButtonEnabled}
-          onPress={onContinuePress}
-          type="circular"
-          style={{
-            backgroundColor: isButtonEnabled
-              ? COLORS.brand500
-              : COLORS.alphaBlack5,
-            marginBottom: verticalScale(48)
-          }}
-        >
+    <SafeAreaView style={{ flex: 1 }}>
+      <KeyboardAvoidingView
+        enabled
+        behavior="padding"
+        style={{ flex: 1, justifyContent: 'space-between' }}
+      >
+        <View style={{ paddingHorizontal: scale(16) }}>
+          <Spacer value={verticalScale(47)} />
           <Text
-            fontSize={16}
-            fontFamily="Inter_600SemiBold"
-            color={isButtonEnabled ? COLORS.neutral0 : COLORS.neutral600}
-            style={{ marginVertical: scale(12) }}
+            align="center"
+            fontSize={24}
+            fontFamily="Inter_700Bold"
+            color={COLORS.neutral800}
           >
-            {t('continue.btn')}
+            {t('confirm.passcode.text')}
           </Text>
-        </Button>
-      </View>
-    </KeyboardAvoidingView>
+          <Spacer value={verticalScale(8)} />
+          <Text
+            align="center"
+            fontSize={16}
+            fontFamily="Inter_500Medium"
+            color={COLORS.neutral800}
+          >
+            {t('confirm.passcode.description')}
+          </Text>
+          <Spacer value={verticalScale(106)} />
+          <Passcode onPasscodeChange={onPasscodeChange} autoFocus={true} />
+          <Spacer value={verticalScale(50)} />
+        </View>
+        <BottomAwareSafeAreaView style={{ paddingHorizontal: scale(16) }}>
+          <Button
+            disabled={!isButtonEnabled}
+            onPress={onContinuePress}
+            type="circular"
+            style={{
+              backgroundColor: isButtonEnabled
+                ? COLORS.brand500
+                : COLORS.alphaBlack5
+            }}
+          >
+            <Text
+              fontSize={16}
+              fontFamily="Inter_600SemiBold"
+              color={isButtonEnabled ? COLORS.neutral0 : COLORS.neutral600}
+              style={{ marginVertical: scale(12) }}
+            >
+              {t('continue.btn')}
+            </Text>
+          </Button>
+        </BottomAwareSafeAreaView>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 };
