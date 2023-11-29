bf0a34f4241fcddae30ea99f4004145f8c68a950
JavidHaji-zada <cavid.hacizade.99@gmail.com>
2023-11-29 17:56:32 +0400

    Subsribe to push notifications of created wallets on new token

--- src/hooks/useAppInit.ts
+++ src/hooks/useAppInit.ts
@@ -2,9 +2,10 @@ import { useEffect, useState } from 'react';
 import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { DeviceService, NotificationService, PermissionService } from '@lib';
-import { CacheableAccount, Permission } from '@appTypes';
+import { CacheableAccount, DatabaseTable, Permission } from '@appTypes';
 import { API } from '@api/api';
 import { Cache, CacheKey } from '@lib/cache';
+import { AccountDBModel, Database } from '@database';
 
 /* eslint camelcase: 0 */
 export const useAppInit = () => {
@@ -36,6 +37,16 @@ export const useAppInit = () => {
           if (watchlist.length > 0) {
             // save under new push token
             API.watcherService.watchAddresses(watchlist.map((w) => w.address));
+          }
+
+          // watch created wallets
+          const allAccounts = (await Database.query(
+            DatabaseTable.Accounts
+          )) as AccountDBModel[];
+          if (allAccounts?.length > 0) {
+            API.watcherService.watchAddresses(
+              allAccounts.map((a) => a.address)
+            );
           }
         } catch (error) {
           // ignore
