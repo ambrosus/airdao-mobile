eb27497cdd030f0ccaddaaf1e1c7efcf65cd7714
JavidHaji-zada <cavid.hacizade.99@gmail.com>
2023-12-01 11:37:01 +0400

    Fix token logo by currency code

--- src/components/modular/TokenLogo/index.tsx
+++ src/components/modular/TokenLogo/index.tsx
@@ -30,26 +30,31 @@ export const TokenLogo = (props: TokenLogoProps) => {
         return <AirdaoWhiteIcon scale={scale} />;
       }
       return <AirdaoBlueIcon scale={scale} />;
-    case AirDAODictTypes.Code.ETH:
+    case AirDAODictTypes.Code.ETH.toLowerCase():
     case 'ethereum':
       return <EthTokenIcon scale={scale} />;
-    case AirDAODictTypes.Code.BUSD:
+    case AirDAODictTypes.Code.BUSD.toLowerCase():
     case 'busd token':
       return <BusdIcon scale={scale} />;
-    case AirDAODictTypes.Code.USDC:
+    case AirDAODictTypes.Code.USDC.toLowerCase():
     case 'usd coin':
       return <UsdcIcon scale={scale} />;
-    case AirDAODictTypes.Code.Tether:
+    case AirDAODictTypes.Code.Tether.toLowerCase():
     case 'tether usd':
       return <TetherIcon scale={scale} />;
+    case AirDAODictTypes.Code.GanymedePoolToken.toLowerCase():
     case 'ganymede pool token':
       return <GanymedeIcon scale={scale} />;
+    case AirDAODictTypes.Code.PlutusPoolToken.toLowerCase():
     case 'plutus pool token':
       return <PlutusIcon scale={scale} />;
+    case AirDAODictTypes.Code.HeraPoolToken.toLowerCase():
     case 'hera pool token':
       return <HeraPoolIcon scale={scale} />;
+    case AirDAODictTypes.Code.FirepotLp.toLowerCase():
     case 'firepot-lp-token':
       return <FirepotIcon scale={scale} />;
+    case AirDAODictTypes.Code.Bond.toLowerCase():
     case 'airbond':
       return <AirBondIcon scale={scale} />;
     default:
