ebbfaba155ac37cfbc640bd7e8df89c25c2d6632
JavidHaji-zada <cavid.hacizade.99@gmail.com>
2023-12-13 16:46:57 +0400

    OTA Update: 1.1.0.12

--- src/screens/Settings/Settings.tsx
+++ src/screens/Settings/Settings.tsx
@@ -78,7 +78,7 @@ export const SettingsScreen = () => {
       <View style={styles.innerContainer}>
         {SETTINGS_MENU_ITEMS.map(renderMenu)}
       </View>
-      {isStage && <Text style={{ margin: 20 }}>Build: 1.1.0.11</Text>}
+      {isStage && <Text style={{ margin: 20 }}>Build: 1.1.0.12</Text>}
     </View>
   );
 };
