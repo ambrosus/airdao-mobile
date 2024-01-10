fe4bad9f640745b9e32ef6003f3bf35983e7468d
JavidHaji-zada <cavid.hacizade.99@gmail.com>
2024-01-10 15:00:39 +0400

    added total amb value

--- src/hooks/useUSDPrice.ts
+++ src/hooks/useUSDPrice.ts
@@ -2,6 +2,12 @@ import { useEffect, useState } from 'react';
 import { AirDAODictTypes } from '@crypto/common/AirDAODictTypes';
 import { useCurrencyRate } from './query/useCurrencyRate';
 
+/**
+ *
+ * @param {number} etherAmount amount of tokens
+ * @param {AirDAODictTypes.Code} symbol Symbol of crypto token to convert into USD.  (Default value: AMB)
+ * @returns {number} USD price of given amount of tokens
+ */
 export const useUSDPrice = (
   etherAmount: number,
   symbol: AirDAODictTypes.Code = AirDAODictTypes.Code.AMB
