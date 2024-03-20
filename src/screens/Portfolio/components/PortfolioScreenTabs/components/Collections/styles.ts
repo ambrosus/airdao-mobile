2223483e4584d400d4dc243c73595e7d4aad92a6
evgeniyjb <evgeniyjb7@gmail.com>
2024-03-20 19:36:15 +0100

    3250 bugfix with the switch in the "Watchlist"

---
+++ src/screens/Portfolio/components/PortfolioScreenTabs/components/Collections/styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    marginTop: 1
+  },
+  spinerWrapper: { flex: 1, justifyContent: 'center', alignItems: 'center' }
+});
