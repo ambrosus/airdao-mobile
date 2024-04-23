a597658c8d42fd86027665b06796c69b35146325
ArthurHoncharuk <Dotwork.Play@gmail.com>
2024-04-23 11:07:51 +0300

    [AMB-3187] chore: resolve typescript conflicts

--- src/api/staking/staking-service.ts
+++ src/api/staking/staking-service.ts
@@ -27,7 +27,8 @@ class Staking {
 
   async createStakingPoolContract(
     addressHash: string,
-    providerOrSigner = this.provider
+    providerOrSigner: ethers.providers.JsonRpcProvider | ethers.Wallet = this
+      .provider
   ) {
     return new ethers.Contract(addressHash, poolAbi, providerOrSigner);
   }
