9d53c59bed47687635381c468d25e88c9d86e9c0
ArthurHoncharuk <Dotwork.Play@gmail.com>
2024-04-24 11:52:41 +0300

    [AMB-3577] fix: reorder staking pools & remove HPT pool

--- src/screens/StakingPools/index.tsx
+++ src/screens/StakingPools/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useMemo, useState } from 'react';
+import * as Updates from 'expo-updates';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useTranslation } from 'react-i18next';
 import { useNavigation } from '@react-navigation/native';
@@ -41,7 +42,11 @@ export const StakingPoolsScreen = () => {
   }, [selectedWallet, fetchPoolDetails]);
 
   const filterStakingPools = useMemo(() => {
-    return stakingPools;
+    return Updates.channel === 'testnet'
+      ? stakingPools.sort((a, b) => Number(b.isActive) - Number(a.isActive))
+      : stakingPools
+          .filter((pool) => pool.token.symbol !== 'GPT')
+          .sort((a, b) => Number(b.isActive) - Number(a.isActive));
   }, [stakingPools]);
 
   const spinnerContainerStyle: StyleProp<ViewStyle> = {
