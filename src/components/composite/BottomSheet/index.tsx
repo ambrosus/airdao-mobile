f8a4a549bd9238772500bd458b0ba7a0f9e670dc
ArthurHoncharuk <Dotwork.Play@gmail.com>
2024-04-27 01:44:39 +0300

    [AMB-2579] fix: resolve refs bug with missing notifications & show toast notifications behind modals & dismiss modal on backdrop press

--- src/components/composite/BottomSheet/index.tsx
+++ src/components/composite/BottomSheet/index.tsx
@@ -5,7 +5,12 @@ import React, {
   useMemo,
   useState
 } from 'react';
-import { Keyboard, Platform, View } from 'react-native';
+import {
+  Keyboard,
+  Platform,
+  TouchableWithoutFeedback,
+  View
+} from 'react-native';
 import Modal from 'react-native-modal';
 import { styles } from './BottomSheet.styles';
 import { BottomSheetProps, BottomSheetRef } from './BottomSheet.types';
@@ -115,16 +120,18 @@ export const BottomSheet = React.forwardRef<BottomSheetRef, BottomSheetProps>(
     const renderBackdropComponent = useMemo(() => {
       const backdropOpacity = isNestedSheet ? 0 : 0.5;
       return (
-        <View
-          style={{
-            ...styles.backdrop,
-            backgroundColor: `rgba(0, 0, 0, ${backdropOpacity})`
-          }}
-        >
-          <Toast />
-        </View>
+        <TouchableWithoutFeedback onPress={dismiss}>
+          <View
+            style={{
+              ...styles.backdrop,
+              backgroundColor: `rgba(0, 0, 0, ${backdropOpacity})`
+            }}
+          >
+            <Toast />
+          </View>
+        </TouchableWithoutFeedback>
       );
-    }, [isNestedSheet]);
+    }, [isNestedSheet, dismiss]);
 
     return (
       <Modal
