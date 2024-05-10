f5a27606d0fd8a8a6c243de6075fd11ef40665d6
ArthurHoncharuk <Dotwork.Play@gmail.com>
2024-05-10 12:20:51 +0300

    [AMB-3764] ui: resolve blinking of modal & chosen selectedWallet at StakingPools

--- src/components/composite/BottomSheet/index.tsx
+++ src/components/composite/BottomSheet/index.tsx
@@ -121,7 +121,7 @@ export const BottomSheet = React.forwardRef<BottomSheetRef, BottomSheetProps>(
       const onBackdropPress = (): void => {
         if (closeOnBackPress) dismiss();
       };
-        
+
       const backdropOpacity = !isVisible ? 0 : isNestedSheet ? 0 : 0.5;
       return (
         <TouchableWithoutFeedback onPress={onBackdropPress}>
@@ -135,7 +135,7 @@ export const BottomSheet = React.forwardRef<BottomSheetRef, BottomSheetProps>(
           </View>
         </TouchableWithoutFeedback>
       );
-    }, [isNestedSheet, dismiss, closeOnBackPress]);
+    }, [isVisible, isNestedSheet, closeOnBackPress, dismiss]);
 
     return (
       <Modal
