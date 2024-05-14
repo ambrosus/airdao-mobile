7d3a5fb6806fccfbf34572af9d545aeab8dd6a9d
ArthurHoncharuk <Dotwork.Play@gmail.com>
2024-05-14 16:20:56 +0300

    [AMB-3730] feat: add password require delay feature with comparing dates

--- src/hooks/usePasscodeEntryRevealer.ts
+++ src/hooks/usePasscodeEntryRevealer.ts
@@ -1,58 +1,83 @@
-import { useCallback, useEffect } from 'react';
-import { Platform } from 'react-native';
+import { useCallback, useEffect, useRef } from 'react';
+import { AppState, AppStateStatus } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { AirDAOEventType, RootNavigationProp } from '@appTypes';
-import { useAppState } from './useAppState';
-import usePasscode from '@contexts/Passcode';
 import { Cache, CacheKey } from '@lib/cache';
 import { AirDAOEventDispatcher } from '@lib';
 import { useCurrentRoute } from '@contexts';
+import usePasscode from '@contexts/Passcode';
 
-const routesToIgnorePasscodeEntry = ['SuccessSetupSecurity', 'ChangePasscode'];
+const APP_HIDDEN_STATES = ['inactive', 'background'];
+const REQUIRE_DELAY_IN_SECONDS = __DEV__ ? 1000 : 2 * 60 * 1000;
+
+const EXCLUDED_PASSCODE_ROUTES = ['SuccessSetupSecurity', 'ChangePasscode'];
 
 export const usePasscodeEntryRevealer = () => {
   const navigation = useNavigation<RootNavigationProp>();
   const currentRoute = useCurrentRoute();
-  const { prevState, appState } = useAppState();
-  const {
-    isPasscodeEnabled,
-    isFaceIDEnabled,
-    loading,
-    isRequestingPermission
-  } = usePasscode();
-  const processPasscodeReveal = useCallback(async () => {
-    const isBiometricAuthenticationInProgress = await Cache.getItem(
-      CacheKey.isBiometricAuthenticationInProgress
-    );
-    // all IOS devices and some Android devices triggers inactive app state whenever we request a biometric authentication (faceId, fingerprint or iris). This state in cache is a workaround to make sure that we do not fire passcode entry state during those transitions.
-    if (isBiometricAuthenticationInProgress) {
-      if (appState === 'inactive' && prevState === 'active') return;
-      await Cache.setItem(CacheKey.isBiometricAuthenticationInProgress, false);
-      return;
-    }
-    if (prevState === 'active' && (isFaceIDEnabled || isPasscodeEnabled)) {
-      // Our BottomSheet component is rendered on top of all screens,meaning that if any modal that is open stays open during this transition,
-      // so we close all open instance of BottomSheet navigating to Passcode
-      AirDAOEventDispatcher.dispatch(AirDAOEventType.CloseAllModals, null);
-      setTimeout(() => {
-        navigation.navigate('Passcode');
-      }, 500);
-    }
-  }, [appState, isFaceIDEnabled, isPasscodeEnabled, navigation, prevState]);
+  const { isPasscodeEnabled, isFaceIDEnabled, loading } = usePasscode();
 
-  const ignoreRequestingPassword = Platform.select({
-    android: !isRequestingPermission && prevState !== 'active',
-    ios: !isRequestingPermission
-  });
+  const prevAppState = useRef<AppStateStatus>(AppState.currentState);
+  const timestampWhenFocused = useRef<number | null>(null);
+
+  const handleAppStateChange = useCallback(
+    async (nextAppState: AppStateStatus) => {
+      if (nextAppState === 'active') {
+        if (APP_HIDDEN_STATES.includes(prevAppState.current)) {
+          const currentTime = new Date().getTime();
+
+          // Findes difference between app state changed and redirect or return to correct screen
+          const isTimestampDiffHaveRevealPass = timestampWhenFocused.current
+            ? currentTime - timestampWhenFocused.current >
+              REQUIRE_DELAY_IN_SECONDS
+            : false;
+
+          await Cache.setItem(
+            CacheKey.isBiometricAuthenticationInProgress,
+            false
+          );
+
+          if (isTimestampDiffHaveRevealPass) {
+            if (isFaceIDEnabled || isPasscodeEnabled) {
+              AirDAOEventDispatcher.dispatch(
+                AirDAOEventType.CloseAllModals,
+                null
+              );
+              setTimeout(() => {
+                navigation.navigate('Passcode');
+              }, 500);
+            }
+          }
+        }
+      }
+      prevAppState.current = nextAppState;
+    },
+    [isFaceIDEnabled, isPasscodeEnabled, navigation]
+  );
 
   useEffect(() => {
-    if (
-      !loading &&
-      !routesToIgnorePasscodeEntry.includes(currentRoute) &&
-      ignoreRequestingPassword
-    ) {
-      processPasscodeReveal();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [navigation, prevState, loading, processPasscodeReveal]);
+    // Final implementation of navigation & listener
+    const appStateChangeListener = (nextAppState: AppStateStatus) => {
+      if (!loading && !EXCLUDED_PASSCODE_ROUTES.includes(currentRoute)) {
+        handleAppStateChange(nextAppState);
+      }
+    };
+
+    AppState.addEventListener('change', appStateChangeListener);
+  }, [currentRoute, loading, handleAppStateChange]);
+
+  // Effect with app state listener & setting date when focused/hidden
+  useEffect(() => {
+    const focusListener = (nextAppState: AppStateStatus) => {
+      if (APP_HIDDEN_STATES.includes(nextAppState)) {
+        timestampWhenFocused.current = new Date().getTime();
+      } else if (nextAppState === 'active') {
+        timestampWhenFocused.current = null;
+      }
+    };
+
+    AppState.addEventListener('change', focusListener);
+  }, []);
+
+  return null;
 };
