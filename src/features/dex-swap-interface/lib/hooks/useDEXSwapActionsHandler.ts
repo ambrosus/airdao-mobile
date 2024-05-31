d67ba95dff33cd355e7dc5e0a26895dfa4cf78a7
ArturHoncharuk <Dotwork.Play@gmail.com>
2024-05-31 15:48:22 +0300

    [AMB-3888] chore(hooks): remove unused params at useDEXSwapActionsHandler

--- src/features/dex-swap-interface/lib/hooks/useDEXSwapActionsHandler.ts
+++ src/features/dex-swap-interface/lib/hooks/useDEXSwapActionsHandler.ts
@@ -1,17 +1,17 @@
 import { useCallback } from 'react';
-import { TokenInfo } from '@features/dex-swap-interface/types';
+
 import { supportedSwapPairs } from '@features/dex-swap-interface/utils/pairs';
 
 interface ButtonActionStringArgs {
-  from: TokenInfo;
-  to: TokenInfo;
+  from: string;
+  to: string;
 }
 
 export function useDEXSwapActionsHandler() {
   const buttonActionString = useCallback(
     ({ from, to }: ButtonActionStringArgs) => {
       const foundPair = supportedSwapPairs.find((token) => {
-        return token.from === from.symbol && token.to === to.symbol;
+        return token.from === from && token.to === to;
       });
 
       if (!foundPair) {
