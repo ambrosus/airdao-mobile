f958de609f56a88419e2d311f2d3c699bd6b31e8
ArturHoncharuk <dotwork.play@gmail.com>
2024-06-12 23:01:36 +0300

    [AMB-3889] ui: add base contract instances

--- src/features/swap/utils/contracts/instances.ts
+++ src/features/swap/utils/contracts/instances.ts
@@ -1,6 +1,6 @@
 import Config from '@constants/config';
-import { Signer, ethers } from 'ethers';
+import { ethers } from 'ethers';
 
-export function createRouterContract(provider) {
+export function createRouterContract(provider: any) {
   return new ethers.Contract(Config.ROUTER_V2_ADDRESS, [], provider);
 }
