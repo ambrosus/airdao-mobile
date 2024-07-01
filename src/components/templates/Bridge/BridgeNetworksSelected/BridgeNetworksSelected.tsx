271b00ba6f6054d9cd1cb7240d8a0c3f6fa10a6e
evgeniyjb <evgeniyjb7@gmail.com>
2024-07-01 17:01:08 +0200

    [AMB-4004_4050]

--- src/components/templates/Bridge/BridgeNetworksSelected/BridgeNetworksSelected.tsx
+++ src/components/templates/Bridge/BridgeNetworksSelected/BridgeNetworksSelected.tsx
@@ -1,21 +1,27 @@
 import React, { useMemo } from 'react';
 import { View } from 'react-native';
 import { styles } from './styles';
-import { Row, Text } from '@components/base';
+import { Row, Spacer, Text } from '@components/base';
 import { TokenLogo } from '@components/modular';
 import { NETWORK, SHORTEN_NETWORK } from '@utils/bridge';
 import { RightArrowIcon } from '@components/svg/icons';
 import { COLORS } from '@constants/colors';
+import { scale } from '@utils/scaling';
+import { useTranslation } from 'react-i18next';
 
 interface BridgeNetworksSelectedProps {
+  type?: 'preview';
   networkFrom: string;
   networkTo: string;
 }
 
 export const BridgeNetworksSelected = ({
+  type,
   networkFrom,
   networkTo
 }: BridgeNetworksSelectedProps) => {
+  const { t } = useTranslation();
+
   const transformedNetworkName = useMemo(() => {
     return {
       from: SHORTEN_NETWORK[networkFrom as keyof typeof NETWORK],
@@ -23,35 +29,59 @@ export const BridgeNetworksSelected = ({
     };
   }, [networkFrom, networkTo]);
 
+  const isPreview = type === 'preview';
+  const wrapperStyle = isPreview
+    ? {
+        ...styles.previewWrapper,
+        ...styles.rowGap
+      }
+    : styles.rowGap;
+
   return (
     <Row alignItems="center" justifyContent="space-evenly">
-      <Row style={styles.rowGap}>
-        <TokenLogo
-          scale={networkFrom === 'amb' ? 0.6 : 0.65}
-          token={networkFrom}
-          overrideIconVariants={{ eth: 'blue' }}
-        />
-
-        <Text fontSize={14} fontFamily="Inter_600SemiBold" color="#1D1D1D">
-          {transformedNetworkName.from}
-        </Text>
-      </Row>
+      <View>
+        {isPreview && (
+          <>
+            <Text>{t('common.transaction.from')}</Text>
+            <Spacer value={scale(10)} />
+          </>
+        )}
+        <Row style={wrapperStyle}>
+          <TokenLogo
+            scale={networkFrom === 'amb' ? 0.6 : 0.65}
+            token={networkFrom}
+            overrideIconVariants={{ eth: 'blue' }}
+          />
+          <Text fontSize={14} fontFamily="Inter_600SemiBold" color="#1D1D1D">
+            {transformedNetworkName.from}
+          </Text>
+        </Row>
+      </View>
 
       <View style={styles.reorder}>
         <RightArrowIcon color={COLORS.black} />
       </View>
 
-      <Row style={styles.rowGap}>
-        <TokenLogo
-          scale={networkTo === 'amb' ? 0.6 : 0.65}
-          token={networkTo}
-          overrideIconVariants={{ eth: 'blue' }}
-        />
+      <View>
+        {isPreview && (
+          <>
+            <Text>{t('common.transaction.to')}</Text>
+            <Spacer value={scale(10)} />
+          </>
+        )}
 
-        <Text fontSize={14} fontFamily="Inter_600SemiBold" color="#1D1D1D">
-          {transformedNetworkName.to}
-        </Text>
-      </Row>
+        <Row style={wrapperStyle}>
+          <TokenLogo
+            scale={networkTo === 'amb' ? 0.6 : 0.65}
+            token={networkTo}
+            overrideIconVariants={{ eth: 'blue' }}
+          />
+
+          <Text fontSize={14} fontFamily="Inter_600SemiBold" color="#1D1D1D">
+            {transformedNetworkName.to}
+          </Text>
+        </Row>
+      </View>
     </Row>
   );
 };
--- src/components/templates/Bridge/BridgeNetworksSelected/styles.ts
+++ src/components/templates/Bridge/BridgeNetworksSelected/styles.ts
@@ -3,12 +3,18 @@ import { scale } from '@utils/scaling';
 import { COLORS } from '@constants/colors';
 
 export const styles = StyleSheet.create({
+  previewWrapper: {
+    paddingHorizontal: scale(12),
+    paddingVertical: scale(10),
+    borderRadius: scale(15),
+    backgroundColor: COLORS.neutral100
+  },
   rowGap: {
     gap: scale(8)
   },
   reorder: {
-    width: scale(40),
-    height: scale(40),
+    width: scale(32),
+    height: scale(32),
     borderRadius: scale(40),
     justifyContent: 'center',
     alignItems: 'center',
