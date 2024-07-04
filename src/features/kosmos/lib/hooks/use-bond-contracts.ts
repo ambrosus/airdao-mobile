410b170e0383e58bb5084ca09e56d92ed35bb3d9
ArturHoncharuk <Dotwork.Play@gmail.com>
2024-07-04 14:16:09 +0300

    [AMB-4087] chore: add bond sdk declarator & patch-package

---
+++ src/features/kosmos/lib/hooks/use-bond-contracts.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import * as AirBondContracts from '@airdao/airdao-bond';
+import { ethers } from 'ethers';
+import { useBridgeContextData } from '@contexts/Bridge';
+import { Cache, CacheKey } from '@lib/cache';
+
+export function useBondContracts() {
+  const { selectedAccount } = useBridgeContextData();
+  const [contracts, setContracts] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const privateKey = (await Cache.getItem(
+          // @ts-ignore
+          `${CacheKey.WalletPrivateKey}-${selectedAccount._raw?.hash}`
+        )) as string;
+
+        const signer = new ethers.Wallet(privateKey);
+        const _contracts = new AirBondContracts.Contracts(signer, 16718);
+
+        if (_contracts) setContracts(_contracts);
+      } catch (error) {
+        throw error;
+      }
+    })();
+  }, [selectedAccount]);
+
+  return { contracts };
+}
---
+++ src/features/kosmos/types/airdao-bond.d.ts
@@ -0,0 +1,3 @@
+declare module '@airdao/airdao-bond' {
+  export const Contracts: any;
+}
