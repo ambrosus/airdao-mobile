274a7cb3e663912cd2753c8b7a44ccee689230fd
ArturHoncharuk <Dotwork.Play@gmail.com>
2024-07-29 13:40:53 +0300

    [AMB-4186] docs: add ERC20 methods JSDoc description

--- src/lib/erc20/erc20.contracts.ts
+++ src/lib/erc20/erc20.contracts.ts
@@ -4,15 +4,38 @@ import Config from '@constants/config';
 import { ERC20_ABI } from './abi/ERC20_ABI';
 
 class ERC20 {
-  createNativeProvider() {
+  /**
+   * Creates an instance of ethers.providers.JsonRpcProvider using the network URL from the configuration.
+   *
+   * @returns {ethers.providers.JsonRpcProvider} The JSON-RPC provider.
+   */
+  createNativeProvider(): ethers.providers.JsonRpcProvider {
     return new ethers.providers.JsonRpcProvider(Config.NETWORK_URL);
   }
 
-  createSigner(privateKey: string) {
+  /**
+   * Creates an instance of ethers.Wallet using the provided private key and the native provider.
+   *
+   * @param {string} privateKey - The private key to create the signer.
+   * @returns {ethers.Wallet} The signer.
+   */
+  createSigner(privateKey: string): ethers.Wallet {
     return new ethers.Wallet(privateKey, this.createNativeProvider());
   }
 
-  async balanceOf({ tokenAddress, ownerAddress }: GetBalanceArgs) {
+  /**
+   * Retrieves the balance of a given token or native currency for a specified owner address.
+   *
+   * @param {GetBalanceArgs} args - The arguments for retrieving the balance.
+   * @param {string} args.tokenAddress - The address of the token contract or the native currency address.
+   * @param {string} args.ownerAddress - The address of the owner whose balance is to be retrieved.
+   * @returns {Promise<ethers.BigNumber>} The balance of the specified address.
+   * @throws Will throw an error if unable to retrieve the balance.
+   */
+  async balanceOf({
+    tokenAddress,
+    ownerAddress
+  }: GetBalanceArgs): Promise<ethers.BigNumber> {
     try {
       const provider = this.createNativeProvider();
 
@@ -26,13 +49,32 @@ class ERC20 {
     }
   }
 
+  /**
+   * Checks the allowance of a given token for a specified spender address.
+   *
+   * @param {GetAllowanceArgs} args - The arguments for checking the allowance.
+   * @param {string} args.tokenAddress - The address of the token contract.
+   * @param {string} args.privateKey - The private key to create the signer.
+   * @param {string} args.amount - The amount to check the allowance against.
+   * @param {string} args.spenderAddress - The address of the spender.
+   * @returns {Promise<{ allowance: ethers.BigNumber, needToIncrease: boolean }>} The allowance and whether it needs to be increased.
+   * @throws Will throw an error if unable to check the allowance.
+   */
   async checkAllowance({
     tokenAddress,
     privateKey,
     amount,
     spenderAddress
-  }: GetAllowanceArgs) {
-    if (tokenAddress === ethers.constants.AddressZero) return false;
+  }: GetAllowanceArgs): Promise<{
+    allowance: ethers.BigNumber;
+    needToIncrease: boolean;
+  }> {
+    if (tokenAddress === ethers.constants.AddressZero)
+      return {
+        allowance: ethers.BigNumber.from('100000000000000'),
+        needToIncrease: false
+      };
+
     try {
       const bnAmount = ethers.utils.parseEther(amount);
 
@@ -56,12 +98,23 @@ class ERC20 {
     }
   }
 
+  /**
+   * Sets the allowance of a given token for a specified spender address.
+   *
+   * @param {SetAllowanceArgs} args - The arguments for setting the allowance.
+   * @param {string} args.tokenAddress - The address of the token contract.
+   * @param {string} args.privateKey - The private key to create the signer.
+   * @param {string} args.amount - The amount to set as the allowance.
+   * @param {string} args.spenderAddress - The address of the spender.
+   * @returns {Promise<ethers.ContractTransaction>} The transaction response of the approve call.
+   * @throws Will throw an error if unable to set the allowance.
+   */
   async setAllowance({
     tokenAddress,
     privateKey,
     amount,
     spenderAddress
-  }: SetAllowanceArgs) {
+  }: SetAllowanceArgs): Promise<ethers.ContractTransaction> {
     try {
       const bnAmount = ethers.utils.parseEther(amount);
       const signer = this.createSigner(privateKey);
