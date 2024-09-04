02f6b0039c6bfc77de1d1ddf3343f1caa3f85e0d
evgeniyjb <evgeniyjb7@gmail.com>
2024-09-04 11:30:36 +0200

    add chain ids to Config

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -37,7 +37,8 @@ const envs = {
     ...BRIDGE_DATA.prod,
     SWAP_TOKENS: SWAP_SUPPORTED_TOKENS.tokens.prod,
     ROUTER_V2_ADDRESS: '0xf7237C595425b49Eaeb3Dc930644de6DCa09c3C4',
-    FACTORY_ADDRESS: '0x2b6852CeDEF193ece9814Ee99BE4A4Df7F463557'
+    FACTORY_ADDRESS: '0x2b6852CeDEF193ece9814Ee99BE4A4Df7F463557',
+    CHAIN_ID: 16718
   },
   stage: {
     WALLET_API_URL: 'https://wallet-api.ambrosus.io',
@@ -72,7 +73,8 @@ const envs = {
     SWAP_TOKENS: SWAP_SUPPORTED_TOKENS.tokens.prod,
     ROUTER_V2_ADDRESS: '0xf7237C595425b49Eaeb3Dc930644de6DCa09c3C4',
     FACTORY_ADDRESS: '0x2b6852CeDEF193ece9814Ee99BE4A4Df7F463557',
-    MARKETPLACE_URL: 'https://bond-backend-api.ambrosus.io'
+    MARKETPLACE_URL: 'https://bond-backend-api.ambrosus.io',
+    CHAIN_ID: 16718
   },
   testnet: {
     WALLET_API_URL: 'https://wallet-api.ambrosus-test.io',
@@ -107,7 +109,8 @@ const envs = {
     SWAP_TOKENS: SWAP_SUPPORTED_TOKENS.tokens.testnet,
     ROUTER_V2_ADDRESS: '0xA3E524dFc9deA66aE32e81a5E2B4DF24F56e2CBc',
     FACTORY_ADDRESS: '0x7bf4227eDfAA6823aD577dc198DbCadECccbEb07',
-    MARKETPLACE_URL: 'https://bond-backend-api.ambrosus-dev.io'
+    MARKETPLACE_URL: 'https://bond-backend-api.ambrosus-dev.io',
+    CHAIN_ID: 22040
   }
 };
 
