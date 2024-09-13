0487784aaa20a8661d642a3ed8420de960610388
ArturHoncharuk <Dotwork.Play@gmail.com>
2024-09-13 12:37:28 +0300

    feat: remove ability to set symbols at deadline

--- src/features/swap/lib/hooks/use-swap-settings.ts
+++ src/features/swap/lib/hooks/use-swap-settings.ts
@@ -1,8 +1,8 @@
+import { useCallback, useEffect, useRef } from 'react';
 import { useSwapContextSelector } from '@features/swap/context';
 import { Settings, SettingsKeys } from '@features/swap/types';
 import { NumberUtils } from '@utils/number';
 import { StringUtils } from '@utils/string';
-import { useCallback, useEffect, useRef } from 'react';
 
 export function useSwapSettings() {
   const { setSettings, _refSettingsGetter } = useSwapContextSelector();
@@ -12,28 +12,55 @@ export function useSwapSettings() {
     settings.current = _refSettingsGetter;
   }, [_refSettingsGetter]);
 
-  const onChangeSettings = useCallback(
-    (key: SettingsKeys, value: string | boolean) => {
-      if (
-        (key === 'slippageTolerance' || key === 'deadline') &&
-        typeof value === 'string'
-      ) {
-        const decimalsLength = key === 'slippageTolerance' ? 2 : 0;
-        let finalValue = StringUtils.formatNumberInput(value);
-        finalValue = NumberUtils.limitDecimalCount(finalValue, decimalsLength);
-
-        return setSettings({
-          ..._refSettingsGetter,
-          [key]: finalValue
-        });
+  const onChangeDeadline = useCallback(
+    (value: string) => {
+      if (value === '0' || value.includes('.') || value.includes(',')) {
+        return;
       }
+      const finalValue = StringUtils.formatNumberInput(value);
+
+      return setSettings((prevState) => ({
+        ...prevState,
+        deadline: finalValue
+      }));
+    },
+    [setSettings]
+  );
+
+  const onChangeSlippageTolerance = useCallback(
+    (value: string) => {
+      const decimalsLength = 2;
+      let finalValue = StringUtils.formatNumberInput(value);
+      finalValue = NumberUtils.limitDecimalCount(finalValue, decimalsLength);
 
       return setSettings({
         ..._refSettingsGetter,
-        [key]: value
+        slippageTolerance: finalValue
       });
     },
     [_refSettingsGetter, setSettings]
+  );
+
+  const onChangeSettings = useCallback(
+    (key: SettingsKeys, value: string | boolean) => {
+      switch (key) {
+        case 'deadline':
+          return onChangeDeadline(value as string);
+        case 'slippageTolerance':
+          return onChangeSlippageTolerance(value as string);
+        default:
+          return setSettings({
+            ..._refSettingsGetter,
+            [key]: value
+          });
+      }
+    },
+    [
+      _refSettingsGetter,
+      onChangeDeadline,
+      onChangeSlippageTolerance,
+      setSettings
+    ]
   );
 
   return { onChangeSettings, settings, _refSettingsGetter };
