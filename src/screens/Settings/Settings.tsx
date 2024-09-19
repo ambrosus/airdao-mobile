177f45472d36379a6f388cceea96697cb3a70647
evgeniyjb <evgeniyjb7@gmail.com>
2024-09-19 14:51:43 +0200

    [new build settings]

--- src/screens/Settings/Settings.tsx
+++ src/screens/Settings/Settings.tsx
@@ -15,8 +15,8 @@ import { SettingsMenuItem } from './Settings.types';
 import { styles } from './styles';
 import { isAndroid } from '@utils/isPlatform';
 
-const iosBuildVersion = '1.3.4';
-const androidBuildVersion = '1.3.4';
+const iosBuildVersion = '1.3.5';
+const androidBuildVersion = '1.3.5';
 
 const stageBuildVersions = {
   ios: `${iosBuildVersion} (${androidBuildVersion})`,
