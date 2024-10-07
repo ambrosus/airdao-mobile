02cf04e7c1b784d7ccda93b682313cb212044fad
ArturHoncharuk <Dotwork.Play@gmail.com>
2024-10-07 15:11:32 +0300

    tests: add coverage tests for percents text component

---
+++ src/screens/Notifications/components/__tests__/NotificationBox.test.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import '@testing-library/jest-native/extend-expect';
+import { screen, render } from '@testing-library/react-native';
+import { NotificationBox } from '../NotificationBox';
+import type { Notification } from '@models';
+import { NotificationType } from '@appTypes';
+import { COLORS } from '@constants/colors';
+
+type OmitNotification = Omit<Notification, '_id' | 'createdAt'>;
+
+const PositivePriceChangeNotification: OmitNotification = {
+  body: '🚀 AMB Price changed on +0.01%! Current price $0.00873',
+  title: 'Price Alert',
+  type: NotificationType.PriceAlert
+};
+
+const NegativePriceChangeNotification: OmitNotification = {
+  body: '🚀 AMB Price changed on -0.01! Current price $0.00873',
+  title: 'Price Alert',
+  type: NotificationType.PriceAlert
+};
+
+describe('NotificationBox unit test', () => {
+  it('renders the component correctly', () => {
+    render(
+      <NotificationBox
+        notification={
+          PositivePriceChangeNotification as unknown as Notification
+        }
+      />
+    );
+
+    expect(
+      screen.getByTestId('notification_percentage_text')
+    ).toBeOnTheScreen();
+  });
+
+  it('renders correct negative color style for negative price change', () => {
+    render(
+      <NotificationBox
+        notification={
+          NegativePriceChangeNotification as unknown as Notification
+        }
+      />
+    );
+
+    expect(screen.getByTestId('notification_percentage_text')).toHaveStyle({
+      color: COLORS.error400
+    });
+  });
+  it('renders correct positive color style for positive price change', () => {
+    render(
+      <NotificationBox
+        notification={
+          PositivePriceChangeNotification as unknown as Notification
+        }
+      />
+    );
+
+    expect(screen.getByTestId('notification_percentage_text')).toHaveStyle({
+      color: COLORS.success400
+    });
+  });
+});
