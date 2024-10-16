a9a3b8f247658c3fba59b53f50059b4b49712c81
ArturHoncharuk <dotwork.play@gmail.com>
2024-10-16 19:49:25 +0300

    build: add environment handler log

--- src/features/wallet-connect/lib/hooks/use-initialize-wallet-kit.ts
+++ src/features/wallet-connect/lib/hooks/use-initialize-wallet-kit.ts
@@ -3,6 +3,7 @@ import { createWalletKit, walletKit } from '@features/wallet-connect/utils';
 import { useWalletConnectContextSelector } from './use-wallet-connect-context';
 import { EXPO_PUBLIC_REOWN_PROJECT_ID } from '@env';
 import { Alert } from 'react-native';
+import Constants from 'expo-constants';
 
 export function useInitializeWalletKit() {
   const { isWalletKitInitiated, setIsWalletKitInitiated, setActiveSessions } =
@@ -12,6 +13,11 @@ export function useInitializeWalletKit() {
     try {
       Alert.alert(
         EXPO_PUBLIC_REOWN_PROJECT_ID ?? 'no EXPO_PUBLIC_REOWN_PROJECT_ID found`'
+      );
+
+      Alert.alert(
+        'Expo Constants',
+        JSON.stringify(Constants.expoConfig?.extra)
       );
 
       Alert.alert('PROCESS ENV', JSON.stringify(process.env));
