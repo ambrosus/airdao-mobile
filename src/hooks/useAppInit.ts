6adea44d69749773b22f4ceca89f2675218640cd
ArturHoncharuk <dotwork.play@gmail.com>
2024-10-21 10:42:18 +0300

    chore: replace wallet connect initiation on AppInit step

--- src/hooks/useAppInit.ts
+++ src/hooks/useAppInit.ts
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { NotificationService, PermissionService } from '@lib';
-import { useInitializeWalletKit } from '@features/wallet-connect/lib/hooks';
 import {
   CacheableAccount,
   CacheableAccountList,
@@ -20,7 +19,6 @@ import {
 
 /* eslint camelcase: 0 */
 export const useAppInit = () => {
-  useInitializeWalletKit();
   const [isAppReady, setIsAppReady] = useState<boolean>(false);
 
   useEffect(() => {
