ad9d2f48d2c91245f204d9d99d11ae3dc427470a
ArturHoncharuk <73081258+ArturHoncharuk@users.noreply.github.com>
2024-11-15 13:01:40 +0200

    chore: organize staking entity imports

---
+++ src/entities/staking/model/staking-pools.store.ts
@@ -0,0 +1,41 @@
+import { staking } from '@api/staking/staking-service';
+import { ReturnedPoolDetails } from '@api/staking/types';
+import { create } from 'zustand';
+
+interface StakingPoolsStore {
+  isInitialFetching: boolean;
+  isFetching: boolean;
+  pools: ReturnedPoolDetails[];
+  fetchPoolDetails: (
+    selectedWallet: string,
+    isMultiply?: boolean
+  ) => Promise<void>;
+}
+
+type SetState = (
+  partial:
+    | Partial<StakingPoolsStore>
+    | ((state: StakingPoolsStore) => Partial<StakingPoolsStore>)
+) => void;
+
+function toggleLoadingState(set: SetState, isMultiply = false, state: boolean) {
+  isMultiply ? set({ isInitialFetching: state }) : set({ isFetching: state });
+}
+
+export const useStakingPoolsStore = create<StakingPoolsStore>((set) => ({
+  pools: [],
+  isFetching: false,
+  isInitialFetching: false,
+  fetchPoolDetails: async (selectedWallet, isMultiply = false) => {
+    try {
+      toggleLoadingState(set, isMultiply, false);
+      const response = await staking.getStakingPoolsDetails({
+        address: selectedWallet
+      });
+
+      if (response) set({ pools: response });
+    } finally {
+      toggleLoadingState(set, isMultiply, false);
+    }
+  }
+}));
