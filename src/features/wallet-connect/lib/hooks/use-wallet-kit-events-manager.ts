f8c82b5caab9f80dbaec8419f9009f9bddafe9fd
ArturHoncharuk <73081258+ArturHoncharuk@users.noreply.github.com>
2024-11-25 14:07:35 +0200

    feat(wallet-connection): add session_delete event handler & filter disconnected topics

--- src/features/wallet-connect/lib/hooks/use-wallet-kit-events-manager.ts
+++ src/features/wallet-connect/lib/hooks/use-wallet-kit-events-manager.ts
@@ -1,24 +1,24 @@
 import { useCallback, useEffect } from 'react';
+import { InteractionManager } from 'react-native';
 import { supportedChains, walletKit } from '@features/wallet-connect/utils';
 import { Toast, ToastType } from '@components/modular';
 import { useWalletConnectContextSelector } from './use-wallet-connect-context';
 import {
   CONNECT_VIEW_STEPS,
-  SessionAuthenticateEvent,
+  SessionDeleteEvent,
   SessionProposalEvent,
-  SessionRequestEvent,
   WALLET_CLIENT_EVENTS,
   WALLET_CORE_EVENTS
 } from '@features/wallet-connect/types';
 import { useHandleBottomSheetActions } from './use-handle-bottom-sheet-actions';
-import { InteractionManager } from 'react-native';
 import { AirDAOEventDispatcher } from '@lib';
 import { AirDAOEventType } from '@appTypes';
 import Config from '@constants/config';
+import { delay } from '@utils/delay';
 
 export function useWalletKitEventsManager(isWalletKitInitiated: boolean) {
   const { onShowWalletConnectBottomSheet } = useHandleBottomSheetActions();
-  const { onChangeProposal, setWalletConnectStep } =
+  const { onChangeProposal, setWalletConnectStep, setActiveSessions } =
     useWalletConnectContextSelector();
 
   const onSessionProposal = useCallback(
@@ -38,40 +38,31 @@ export function useWalletKitEventsManager(isWalletKitInitiated: boolean) {
         setWalletConnectStep(CONNECT_VIEW_STEPS.WRONG_CHAIN_ERROR);
         onChangeProposal(proposal);
 
-        new Promise<void>((resolve) => setTimeout(resolve, 1000));
+        delay(1000);
         InteractionManager.runAfterInteractions(onShowWalletConnectBottomSheet);
       } else {
         setWalletConnectStep(CONNECT_VIEW_STEPS.APPROVE);
         onChangeProposal(proposal);
 
-        new Promise<void>((resolve) => setTimeout(resolve, 1000));
+        delay(1000);
         InteractionManager.runAfterInteractions(onShowWalletConnectBottomSheet);
       }
     },
     [onChangeProposal, onShowWalletConnectBottomSheet, setWalletConnectStep]
   );
 
-  const onSessionRequest = useCallback((requestEvent: SessionRequestEvent) => {
-    console.warn('onSessionRequest', requestEvent);
-  }, []);
-
-  const onSessionAuthenticate = useCallback(
-    (authEvent: SessionAuthenticateEvent) => {
-      console.warn('onSessionAuthenticate', authEvent);
-    },
-    []
+  const onSessionDelete = useCallback(
+    (event: SessionDeleteEvent) =>
+      setActiveSessions((prevState) =>
+        prevState.filter((session) => session.topic !== event.topic)
+      ),
+    [setActiveSessions]
   );
 
   useEffect(() => {
     if (isWalletKitInitiated) {
-      // Sign Events Handler
       walletKit.on(WALLET_CORE_EVENTS.SESSION_PROPOSAL, onSessionProposal);
-      walletKit.on(WALLET_CORE_EVENTS.SESSION_REQUEST, onSessionRequest);
-      // Auth Events Handler
-      walletKit.on(
-        WALLET_CORE_EVENTS.SESSION_AUTHENTICATE,
-        onSessionAuthenticate
-      );
+      walletKit.on(WALLET_CORE_EVENTS.SESSION_DELETE, onSessionDelete);
 
       walletKit.engine.signClient.events.on(
         WALLET_CLIENT_EVENTS.SESSION_PING,
@@ -83,10 +74,5 @@ export function useWalletKitEventsManager(isWalletKitInitiated: boolean) {
         }
       );
     }
-  }, [
-    isWalletKitInitiated,
-    onSessionAuthenticate,
-    onSessionProposal,
-    onSessionRequest
-  ]);
+  }, [isWalletKitInitiated, onSessionDelete, onSessionProposal]);
 }
