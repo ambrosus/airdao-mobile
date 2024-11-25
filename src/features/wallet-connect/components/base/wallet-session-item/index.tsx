546a69bbd54258aa4e1f9311eccb3ea8d69456f7
ArturHoncharuk <73081258+ArturHoncharuk@users.noreply.github.com>
2024-11-25 15:13:33 +0200

    ui(wallet-connect-modal): add loading state when disconnecting

--- src/features/wallet-connect/components/base/wallet-session-item/index.tsx
+++ src/features/wallet-connect/components/base/wallet-session-item/index.tsx
@@ -1,10 +1,10 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { Pressable } from 'react-native';
 import { useTranslation } from 'react-i18next';
 import { SessionTypes } from '@walletconnect/types';
 import { getSdkError } from '@walletconnect/utils';
 import { styles } from './styles';
-import { Row, Text } from '@components/base';
+import { Row, Spinner, Text } from '@components/base';
 import { COLORS } from '@constants/colors';
 import { walletKit } from '@features/wallet-connect/utils';
 import {
@@ -18,32 +18,30 @@ interface WalletSessionItemProps {
 
 export const WalletSessionItem = ({ connection }: WalletSessionItemProps) => {
   const { t } = useTranslation();
-  const { activeSessions, setActiveSessions } =
-    useWalletConnectContextSelector();
+  const { setActiveSessions } = useWalletConnectContextSelector();
 
   const { onDismissActiveSessionBottomSheet } = useHandleBottomSheetActions();
 
+  const [disconnecting, setDisconnecting] = useState(false);
+
   const onRejectSession = useCallback(async () => {
     try {
+      setDisconnecting(true);
       await walletKit.disconnectSession({
         topic: connection.topic,
         reason: getSdkError('USER_DISCONNECTED')
       });
 
-      const newPairings = activeSessions.filter(
-        (pairing) => pairing.topic !== connection.topic
+      setActiveSessions((prevState) =>
+        prevState.filter((pairing) => pairing.topic !== connection.topic)
       );
-      setActiveSessions(newPairings);
       onDismissActiveSessionBottomSheet();
     } catch (error) {
       throw error;
+    } finally {
+      setDisconnecting(true);
     }
-  }, [
-    activeSessions,
-    connection.topic,
-    onDismissActiveSessionBottomSheet,
-    setActiveSessions
-  ]);
+  }, [connection.topic, onDismissActiveSessionBottomSheet, setActiveSessions]);
 
   return (
     <Pressable>
@@ -63,16 +61,21 @@ export const WalletSessionItem = ({ connection }: WalletSessionItemProps) => {
         </Text>
 
         <Pressable
+          disabled={disconnecting}
           onPress={onRejectSession}
           style={({ pressed }) => [{ opacity: pressed ? 0.5 : 1 }]}
         >
-          <Text
-            fontSize={14}
-            fontFamily="Inter_600SemiBold"
-            color={COLORS.error500}
-          >
-            {t('wallet.connect.button.disconnect')}
-          </Text>
+          {disconnecting ? (
+            <Spinner size="xs" />
+          ) : (
+            <Text
+              fontSize={14}
+              fontFamily="Inter_600SemiBold"
+              color={COLORS.error500}
+            >
+              {t('wallet.connect.button.disconnect')}
+            </Text>
+          )}
         </Pressable>
       </Row>
     </Pressable>
