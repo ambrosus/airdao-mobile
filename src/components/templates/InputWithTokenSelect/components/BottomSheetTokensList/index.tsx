11e34950009fb342d46965dbe759c6cbde2c993c
EvgeniyJB <81742350+EvgeniyJB@users.noreply.github.com>
2024-11-26 11:10:22 +0100

    ui: bugfix on token list modal

--- src/components/templates/InputWithTokenSelect/components/BottomSheetTokensList/index.tsx
+++ src/components/templates/InputWithTokenSelect/components/BottomSheetTokensList/index.tsx
@@ -1,9 +1,8 @@
-import React, { PropsWithChildren, forwardRef, useMemo } from 'react';
+import React, { forwardRef, PropsWithChildren, useMemo } from 'react';
 import { StyleProp, View, ViewStyle } from 'react-native';
 import { styles } from './styles';
 import { BottomSheet, BottomSheetRef } from '@components/composite';
 import { useForwardedRef } from '@hooks';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 interface BottomSheetTokensListProps extends PropsWithChildren {
   title: string;
@@ -14,13 +13,11 @@ export const BottomSheetTokensList = forwardRef<
   BottomSheetRef,
   BottomSheetTokensListProps
 >(({ title, containerStyle, children }, ref) => {
-  const { bottom } = useSafeAreaInsets();
-
   const bottomSheetRef = useForwardedRef(ref);
 
   const combineContainerStyle = useMemo(
-    () => [styles.container, containerStyle, { paddingBottom: bottom }],
-    [bottom, containerStyle]
+    () => [styles.container, containerStyle],
+    [containerStyle]
   );
 
   return (
