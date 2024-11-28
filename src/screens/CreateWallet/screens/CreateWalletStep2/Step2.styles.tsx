2d13023f8d8e0f083bf28ea311abc2842bd032e0
EvgeniyJB <81742350+EvgeniyJB@users.noreply.github.com>
2024-11-28 11:40:04 +0100

    ui:  horisontal padding on mnemonic container

--- src/screens/CreateWallet/screens/CreateWalletStep2/Step2.styles.tsx
+++ src/screens/CreateWallet/screens/CreateWalletStep2/Step2.styles.tsx
@@ -29,7 +29,7 @@ export const styles = StyleSheet.create({
     height: 142,
     alignSelf: 'center',
     alignItems: 'center',
-    columnGap: scale(20)
+    columnGap: scale(14)
   },
   button: {
     position: 'relative',
