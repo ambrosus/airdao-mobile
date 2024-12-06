95bc08c6f27f45c0b3545795492e01586f4bffaf
ArturHoncharuk <73081258+ArturHoncharuk@users.noreply.github.com>
2024-12-06 16:26:55 +0200

    build: add test environment & alert on firebase event

--- src/lib/firebaseEventAnalytics/sendFirebaseEvent.ts
+++ src/lib/firebaseEventAnalytics/sendFirebaseEvent.ts
@@ -1,8 +1,10 @@
 import { CustomAppEvents } from '@lib/firebaseEventAnalytics/constants/CustomAppEvents';
 import analytics from '@react-native-firebase/analytics';
+import { Alert } from 'react-native';
 
 export const sendFirebaseEvent = (event: CustomAppEvents, params?: object) => {
   if (!event || __DEV__) return;
+  Alert.alert(`Handle event -> ${JSON.stringify(event)}`);
   const paramsNotEmpty = params && Object.keys(params);
 
   try {
