d9c43728c92f24ee4f91929c0d814c1aaeb7a273
EvgeniyJB <evgeniyjb7@gmail.com>
2025-01-03 12:26:25 +0100

    ui change background colors

--- src/screens/Harbor/ProcessStake/styles.tsx
+++ src/screens/Harbor/ProcessStake/styles.tsx
@@ -1,11 +1,13 @@
 import { StyleSheet } from 'react-native';
+import { COLORS } from '@constants/colors';
 import { scale } from '@utils';
 
 export const styles = StyleSheet.create({
   main: { height: '100%' },
   wrapper: {
     paddingHorizontal: scale(12),
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    backgroundColor: COLORS.neutral50
   },
   stakeInfoText: {
     paddingHorizontal: scale(2),
