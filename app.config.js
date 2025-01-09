5892e46ec605b7bf32f2e091f391a7325c032291
EvgeniyJB <81742350+EvgeniyJB@users.noreply.github.com>
2025-01-09 13:46:34 +0100

    [new build settings]

--- app.config.js
+++ app.config.js
@@ -28,10 +28,10 @@ module.exports = {
           'Allow $(PRODUCT_NAME) to access your microphone',
         NSFaceIDUsageDescription: 'Allow $(PRODUCT_NAME) to access FaceID'
       },
-      buildNumber: '1.3.14'
+      buildNumber: '1.3.2'
     },
     android: {
-      versionCode: 108,
+      versionCode: 109,
       softwareKeyboardLayoutMode: 'pan',
       adaptiveIcon: {
         foregroundImage: './assets/adaptive-icon.png',
