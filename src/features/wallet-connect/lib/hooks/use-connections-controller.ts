fe210024c092a50c6a4287480d0c5ae708180533
ArturHoncharuk <73081258+ArturHoncharuk@users.noreply.github.com>
2025-01-24 13:19:27 +0200

    refactor(wallet-connect): improve session management and validation
    - Updated `useConnectionsController` to utilize `AppState` for managing active sessions based on app focus, enhancing session handling.
    - Refactored `validateAndFilterSessions` to use `SessionTypes.Struct` for improved type safety and validation logic, ensuring robust session management.

--- src/features/wallet-connect/lib/hooks/use-connections-controller.ts
+++ src/features/wallet-connect/lib/hooks/use-connections-controller.ts
@@ -1,7 +1,8 @@
 import { useEffect, useMemo } from 'react';
+import { AppState } from 'react-native';
 import { useWalletStore } from '@entities/wallet';
-import { useAppFocus } from '@hooks';
 import { useWalletConnectContextSelector } from './use-wallet-connect-context';
+import { walletKit } from '../wc.core';
 
 const EIP155Regex = /^eip155:\d+:/;
 
@@ -9,28 +10,24 @@ export function useConnectionsController() {
   const { wallet } = useWalletStore();
   const { activeSessions, setActiveSessions } =
     useWalletConnectContextSelector();
-  const active = useAppFocus();
 
   useEffect(() => {
-    if (!wallet?.address) return;
+    const subscription = AppState.addEventListener('change', (state) => {
+      if (state === 'active') {
+        setActiveSessions(Object.values(walletKit.getActiveSessions()));
+      }
+    });
 
-    if (active)
-      setActiveSessions((prevState) =>
-        prevState.filter((session) =>
-          session.namespaces.eip155.accounts
-            .map((account) => account.replace(EIP155Regex, ''))
-            .includes(wallet?.address)
-        )
-      );
-  }, [active, setActiveSessions, wallet?.address]);
+    return () => {
+      subscription.remove();
+    };
+  }, [activeSessions, setActiveSessions, wallet?.address]);
 
   return useMemo(() => {
-    if (!wallet?.address) return [];
-
     return activeSessions.filter((session) =>
       session.namespaces.eip155.accounts
         .map((account) => account.replace(EIP155Regex, ''))
-        .includes(wallet?.address)
+        .includes(wallet?.address ?? '')
     );
   }, [activeSessions, wallet?.address]);
 }
