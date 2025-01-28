07b736945e8972f077f3689303e2f5162516d9d3
EvgeniyJB <81742350+EvgeniyJB@users.noreply.github.com>
2025-01-28 10:24:48 +0100

    hotfix: remove unused variables

--- src/components/modular/StakingPool/index.tsx
+++ src/components/modular/StakingPool/index.tsx
@@ -13,20 +13,13 @@ interface StakingPoolItemProps {
   stakingPool: StakingPool;
 }
 
-const HPT_ADDRESS = '0x322269e52800e5094c008f3b01A3FD97BB3C8f5D';
 export const StakingPoolItem = (props: StakingPoolItemProps) => {
   const { stakingPool } = props;
   const poolStakingDetails = useStakingPoolDetails(stakingPool.token.name);
   const { t } = useTranslation();
-  const isHPTToken = stakingPool.token.address === HPT_ADDRESS;
-  const disabledPools = !stakingPool.isActive && !isHPTToken;
 
   return (
-    <Row
-      alignItems="center"
-      justifyContent="space-between"
-      // style={{ opacity: stakingPool.isActive ? 1 : 0.5 }}
-    >
+    <Row alignItems="center" justifyContent="space-between">
       <Row alignItems="center">
         <TokenLogo token={stakingPool.token.name} />
         <Spacer value={scale(12)} horizontal />
