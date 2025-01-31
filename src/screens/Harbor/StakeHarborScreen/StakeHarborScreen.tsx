65781a4fbb3a4b583883287565b097b5c6498828
ArturHoncharuk <73081258+ArturHoncharuk@users.noreply.github.com>
2025-01-31 12:21:19 +0200

    feat: reset Harbor stake tab index on navigation back

--- src/screens/Harbor/StakeHarborScreen/StakeHarborScreen.tsx
+++ src/screens/Harbor/StakeHarborScreen/StakeHarborScreen.tsx
@@ -1,17 +1,21 @@
 import { useEffect } from 'react';
 import { View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { HarborNavigationProp } from '@appTypes/navigation/harbor';
 import { HeaderWithWithdrawal } from '@entities/harbor/components/composite';
-import { useStakeHBRStore } from '@entities/harbor/model';
+import { useStakeHBRStore, useStakeUIStore } from '@entities/harbor/model';
 import { useHarborStore } from '@entities/harbor/model/harbor-store';
 import { useWalletStore } from '@entities/wallet';
 import { HarborStakeTabs } from '@features/harbor/components/tabs';
 import { styles } from './styles';
 
 export const StakeHarborScreen = () => {
+  const navigation = useNavigation<HarborNavigationProp>();
   const { updateAll } = useHarborStore();
   const { hbrYieldFetcher } = useStakeHBRStore();
   const { wallet } = useWalletStore();
+  const { setActiveTabIndex } = useStakeUIStore();
 
   useEffect(() => {
     updateAll(wallet?.address || '');
@@ -19,6 +23,17 @@ export const StakeHarborScreen = () => {
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [wallet?.address]);
+
+  useEffect(() => {
+    // Reset active tab index when going back
+    const unsubscribe = navigation.addListener('beforeRemove', (event) => {
+      if (event.data.action.type === 'GO_BACK') {
+        setActiveTabIndex(0);
+      }
+    });
+
+    return unsubscribe;
+  }, [navigation, setActiveTabIndex]);
 
   return (
     <SafeAreaView>
