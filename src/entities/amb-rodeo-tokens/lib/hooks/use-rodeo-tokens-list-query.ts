982cdd5a280ca6f9247c99841ecc764abc85654a
ArturHoncharuk <73081258+ArturHoncharuk@users.noreply.github.com>
2025-04-16 13:35:30 +0300

    feat(swap): integrate rodeo tokens into liquidity pool and balance management
    - Added useRodeoTokensStore to manage token state across various swap utilities.
    - Updated useAllLiquidityPools to handle token addresses in a case-insensitive manner.
    - Refactored balance initialization to utilize tokens from the rodeo store.
    - Enhanced multi-route generation and address wrapping functions to incorporate rodeo tokens.
    - Integrated useRodeoTokensListQuery in SwapScreen for dynamic token data retrieval.

--- src/entities/amb-rodeo-tokens/lib/hooks/use-rodeo-tokens-list-query.ts
+++ src/entities/amb-rodeo-tokens/lib/hooks/use-rodeo-tokens-list-query.ts
@@ -1,5 +1,6 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useQuery } from '@apollo/client';
+import { useRodeoTokensStore } from '@entities/amb-rodeo-tokens/model';
 import { RodeoToken } from '@entities/amb-rodeo-tokens/types';
 import { ApolloEndpointsKeys } from '@lib';
 import { AMBRODEO_TOKENS } from '../rodeo-tokens.graph';
@@ -9,12 +10,20 @@ type TokensQuery = {
 };
 
 export function useRodeoTokensListQuery() {
+  const { tokens: allRodeoTokens, onChangeRodeoTokens } = useRodeoTokensStore();
+
   const { data, loading } = useQuery<TokensQuery>(AMBRODEO_TOKENS, {
     pollInterval: 60000, // Poll every minute,
     context: { apiName: ApolloEndpointsKeys.AMBRODEO_TOKENS }
   });
 
   const tokens = useMemo(() => data?.tokens || [], [data]);
+
+  useEffect(() => {
+    if (allRodeoTokens.length !== tokens.length) {
+      onChangeRodeoTokens(tokens);
+    }
+  }, [tokens, allRodeoTokens, onChangeRodeoTokens]);
 
   return { tokens, loading };
 }
